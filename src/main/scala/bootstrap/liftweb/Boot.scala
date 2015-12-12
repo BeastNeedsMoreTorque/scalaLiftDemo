7188ffb44628753d662485ab290640daf4ed0055
Philippe Derome <phderome@gmail.com>
2015-12-12 16:59:52 -0500

    Remove blank lines in build.sbt since we're using sufficiently new version, no longer needed.
    Renamed UserProducts to UserProduct
    
    Fixed CreateDate/UpdateDate bug missing for Product (associate with proper object/class when extending)

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -39,7 +39,7 @@ class Boot {
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User, DBProduct, UserProducts) // last args represent our ORM table classes.
+    Schemifier.schemify(true, Schemifier.infoF _, User, DBProduct, UserProduct) // last args represent our ORM table classes.
 
     // where to search snippet
     LiftRules.addToPackages("code")
