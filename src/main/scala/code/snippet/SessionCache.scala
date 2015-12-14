f0297ea6f84e8c24ffe34083e6a2a295ed3f99d9
Philippe Derome <phderome@gmail.com>
2015-12-14 18:25:35 -0500

    webapp: make document.onload start the geolocation from index.html rather than from scala code as JavaScriptTail now irrelevant.
    GetStores can now render a input element with a Store element provided by Session rather than props, a step towards getting it from JS as Ajax form (later on). It finds the Store using Session geolocation.
    
    Category as Session data is now a string rather than a Box[String], which is more natural.
    
    SessionCache. Latitude and Longitude are now Session Var making inter-snippet communication easier. Should be case class to make updates consistent.

--- src/main/scala/code/snippet/SessionCache.scala
+++ src/main/scala/code/snippet/SessionCache.scala
@@ -9,14 +9,19 @@ import net.liftweb.util.Helpers._
   * Created by philippederome on 2015-12-05.
   */
 object SessionCache {
-  private val defaultCategory = Props.get("product.Category")
+  private val defaultCategory = Props.get("product.Category", "wine")
   // None defined is acceptable though not great.
   private val defaultStore = Props.getInt("store.Id", 1)
+  private val defaultLatitude: String = Props.get("user.latitude", "")
+  private val defaultLongitude: String = Props.get("user.longitude", "")
 
   // if 1 it's HWY 401 & Weston.
 
-  object TheCategory extends SessionVar[Box[String]](defaultCategory)
+  object TheCategory extends SessionVar[String](defaultCategory)
 
   object TheStore extends SessionVar[Int](defaultStore)
+
+  object TheLatitude extends SessionVar[String](defaultLatitude)
+  object TheLongitude extends SessionVar[String](defaultLongitude)
 
 }
