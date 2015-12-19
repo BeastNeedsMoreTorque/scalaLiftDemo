700e585ec6704c31aa84c825ca4523112576ce59
Philippe Derome <phderome@gmail.com>
2015-12-18 21:06:12 -0500

    very minor changes.

--- src/main/scala/code/snippet/GetStores.scala
+++ src/main/scala/code/snippet/GetStores.scala
@@ -2,21 +2,20 @@ package code.snippet
 
 import code.model.{GeoCoordinates, Store, StoreProvider}
 import code.snippet.SessionCache.{TheStore, TheUserCoordinates}
-import net.liftweb.http.{S, SHtml}
 import net.liftweb.util.Helpers._
-import net.liftweb.common.{Full, Empty, Box}
+import net.liftweb.common.{Loggable, Full, Empty, Box}
 
 
 /**
   * Created by philippederome on 2015-12-09.
   */
-object GetStores  {
+object GetStores extends Loggable {
   private val provider = new StoreProvider()
 
   def locate(geo: GeoCoordinates): Box[Store] = {
     TheUserCoordinates.set(geo)
 
-    println(s"GetStores.locate $TheUserCoordinates ${TheStore.is}")
+    logger.info(s"GetStores.locate $TheUserCoordinates ${TheStore.is}")
     provider.findStore(geo) match {
       case util.Success(Full(x)) =>
         TheStore.set(x.id)
