5e1c33bce4fadc8635b12943c863e7b29101f422
Philippe Derome <phderome@gmail.com>
2015-12-22 19:20:02 -0500

    extended column for DBProduct to 40 chars (crashed for coolers FIX!) Got rid of all actor/comet code.
    Got rid of with-resource-id wrapping up myscripts.js.
    
    Documented issues further.
    
    Buffered first maySelect in recommend (why?)
    
    ensured cancel cb hides product from display (idea from a presentation on web as opposed to rewriting html markup for that).

--- src/main/scala/code/model/ProductMessage.scala
+++
@@ -1,8 +0,0 @@
-package code.model
-
-import net.liftweb.common.{Loggable, Box, Empty}
-
-/**
-  * Created by philippederome on 2015-12-13.
-  */
-case class ProductMessage(product: Box[Product]= Empty) extends Loggable {}
