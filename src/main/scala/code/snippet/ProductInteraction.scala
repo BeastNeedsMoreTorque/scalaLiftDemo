2b9b63fed44900abd691034cc7327c5ac7fcb84d
Philippe Derome <phderome@gmail.com>
2015-12-23 08:18:03 -0500

    implemented first cut of prodAttributes. Need to clean up in general now!

--- src/main/scala/code/snippet/ProductInteraction.scala
+++ src/main/scala/code/snippet/ProductInteraction.scala
@@ -1,12 +1,11 @@
 package code.snippet
 
 import code.model._
-import code.snippet.SessionCache.{TheCategory, TheStore}
+import code.snippet.SessionCache._
 import net.liftweb.common._
 import net.liftweb.http.js.JE._
 import net.liftweb.http.js.JsCmd
 import net.liftweb.http.js.JsCmds._
-import net.liftweb.http.js.jquery.JqJsCmds.jsExpToJsCmd
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.util.Helpers._
 import net.liftweb.util.{ClearClearable, CssSel, Props}
@@ -14,31 +13,40 @@ import net.liftweb.common.Box
 import scala.xml.Text
 
 /**
-  * This snippet contains state only via HTTP session (A Liftweb snippet is a web page fragment that has autonomous existence equivalent to a page with framework
-  * responsible to render them all and to handle events between snippets and corresponding html div elements) AND displayInstructions representing last data
-  * received asynchronously as a CometListener, which updates this with context how it should render itself, namely enabling/disabling button.
-  * Much html and Javascript is generated here thanks to the capabilities of liftweb. Similar general comments apply more or less for other comet listeners and to snippets.
+  * This snippet contains state only via HTTP session (A Liftweb snippet is a web page fragment that has autonomous existence
+  * equivalent to a page with framework
+  * responsible to render them all and to handle events between snippets and corresponding html div elements)
+  * AND displayInstructions representing last data
+  * received asynchronously as a CometListener, which updates this with context how it should render itself,
+  * namely enabling/disabling button.
+  * Much html and Javascript is generated here thanks to the capabilities of liftweb.
+  * Similar general comments apply more or less for other comet listeners and to snippets.
   * Created by philippederome on 15-10-26.
   */
 class ProductInteraction extends Loggable {
   private val maxSampleSize = Props.getInt("product.maxSampleSize", 10)
   private var product: Box[Product] = Empty // private state indicating whether to show product when one is defined
-  // and in this context here whether to enable or disable the Recommend button (empty means enable button, Full (product) being present means disable).
+  // and in this context here whether to enable or disable the Recommend button
+  // (empty means enable button, Full (product) being present means disable).
   private var confirm = ""
 
   def render = {
     val toggleButtonsToConsumeJS = Call("lcboViewer.toggleButtonPair", "consume", "recommend")
     val toggleButtonsToRecommendJS = Call("lcboViewer.toggleButtonPair","recommend", "consume")
     def setConfirmJS = SetHtml("confirmMsg", Text(confirm))
-    def prodDisplayJS(prod: Product) =  SetHtml("productImg", <img src={prod.imageThumbUrl}/>)
+    def prodAttributesJS(p: Product) = {
+      val nodeSeq = for (x <- p.createProductLIElemVals) yield <li> {x} </li>
+      SetHtml("prodAttributes", nodeSeq)
+    }
+    def prodDisplayJS(prod: Product) =  SetHtml("productImg", <img src={prod.imageThumbUrl}/>) & prodAttributesJS(prod) & JsShowId("prodDisplay")
     val hideProdDisplayJS =  JsHideId("prodDisplay")
-    val showProdDisplayJS = JsShowId("prodDisplay")
     def cancelCbJS = toggleButtonsToRecommendJS & setConfirmJS & hideProdDisplayJS
 
     def recommend(): JsCmd = {
       def maySelect(): JsCmd =
         TheStore.is.id match {
-          // validates expected numeric input TheStore (a http session attribute) and when valid, do real handling of accessing LCBO data
+          // validates expected numeric input TheStore (a http session attribute) and when valid,
+          // do real handling of accessing LCBO data
           case s if s > 0 =>
             val prod = Product.recommend(maxSampleSize, s, TheCategory.is) match {
               // we want to distinguish error messages to user to provide better diagnostics.
@@ -54,16 +62,19 @@ class ProductInteraction extends Loggable {
               Noop
             } { p: Product =>
               product = Full(p)
-              S.clearCurrentNotices
-              prodDisplayJS(p) // clear error message to make way for normal layout representing normal condition.
+              S.clearCurrentNotices // clear error message to make way for normal layout representing normal condition.
+              prodDisplayJS(p)
             }
-          case _ => S.error(s"Enter a number > 0 for Store") // Error goes to site menu, but we could also send it to a DOM element if we were to specify an additional parameter
+          case _ => S.error(s"Enter a number > 0 for Store")
+          // Error goes to site menu, but we could also send it to a DOM element if we were to specify an additional parameter
         }
 
       product match {
-        case Full(p) => prodDisplayJS(p) // ignore consecutive clicks for flow control, ensuring we take only the user's first click as actionable for a series of clicks on button before we have time to disable it
-        case _ =>
-          maySelect() & toggleButtonsToConsumeJS & showProdDisplayJS & setConfirmJS  // normal processing  (ProductConsume does it the other way around as it plays opposite role as to when it should be active)
+        case Full(p) => prodDisplayJS(p)
+        // ignore consecutive clicks for flow control, ensuring we take only the user's first click as actionable
+        // for a series of clicks on button before we have time to disable it
+        case _ => maySelect() & toggleButtonsToConsumeJS & setConfirmJS
+        // normal processing  (ProductConsume does it the other way around as it plays opposite role as to when it should be active)
       }
     }
 
@@ -79,8 +90,9 @@ class ProductInteraction extends Loggable {
       }
 
       product match {
-        case Full(p)  =>
-          mayConsume(p) & toggleButtonsToRecommendJS & hideProdDisplayJS & setConfirmJS  // we got notified that we have a product that can be consumed and user expressed interest in consuming. So, try it as a simulation by doing a DB store.
+        case Full(p)  => mayConsume(p) & toggleButtonsToRecommendJS & hideProdDisplayJS & setConfirmJS
+          // we got notified that we have a product that can be consumed and user expressed interest in consuming.
+          // So, try it as a simulation by doing a DB store.
         case _ => Noop // ignore consecutive clicks, ensuring we do not attempt to consume multiple times in a row on a string of clicks from user
       }
     }
@@ -101,7 +113,7 @@ class ProductInteraction extends Loggable {
     }
     val addConsumeImgElem: CssSel = "@consume *+" #> <img src="/images/winesmall.png" alt="consume represented as glass of wine"/>
     val addRecommendImgElem: CssSel = "@recommend *+" #> <img src="/images/recommend.png" alt="recommend represented as question mark"/>
-    val addCancelImgElem: CssSel = "@cancel *+" #> <img src="/images/cancel.png" alt="cancel represented as X"/>  // since above completely wiped out the XML (HTML5) node, we just lost the nice image with button, so add it back by adding a DOM element with img underneath the button.
+    val addCancelImgElem: CssSel = "@cancel *+" #> <img src="/images/cancel.png" alt="cancel represented as X"/>
 
     "* [onclick]" #> SHtml.ajaxCall(JsRaw("this.value"), { (s: String) =>
        s match {
