7b0153a7231973b60e0d9e49dc478c9d9d8a2c1f
Philippe Derome <phderome@gmail.com>
2015-12-23 18:05:02 -0500

    made ProductInteraction an object, not a class, and stateless.

--- src/main/scala/code/snippet/ProductInteraction.scala
+++ src/main/scala/code/snippet/ProductInteraction.scala
@@ -9,38 +9,37 @@ import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.util.Helpers._
 import net.liftweb.util.{ClearClearable, CssSel, Props}
-import net.liftweb.common.Box
 import scala.xml.Text
 
 /**
   * This snippet contains state only via HTTP session (A Liftweb snippet is a web page fragment that has autonomous existence
   * equivalent to a page with framework
-  * responsible to render them all and to handle events between snippets and corresponding html div elements)
-  * AND displayInstructions representing last data
-  * received asynchronously as a CometListener, which updates this with context how it should render itself,
-  * namely enabling/disabling button.
+  * responsible to render all snippets on a page and to handle events between snippets and corresponding html div elements)
   * Much html and Javascript is generated here thanks to the capabilities of liftweb.
-  * Similar general comments apply more or less for other comet listeners and to snippets.
   * Created by philippederome on 15-10-26.
   */
-class ProductInteraction extends Loggable {
+object ProductInteraction extends Loggable {
   private val maxSampleSize = Props.getInt("product.maxSampleSize", 10)
-  private var product: Box[Product] = Empty // private state indicating whether to show product when one is defined
-  // and in this context here whether to enable or disable the Recommend button
-  // (empty means enable button, Full (product) being present means disable).
-  private var confirm = ""
 
   def render = {
     val toggleButtonsToConsumeJS = Call("lcboViewer.toggleButtonPair", "consume", "recommend")
     val toggleButtonsToRecommendJS = Call("lcboViewer.toggleButtonPair","recommend", "consume")
-    def setConfirmJS = SetHtml("confirmMsg", Text(confirm))
+    def setConfirmJS = SetHtml("confirmMsg", Text(TheSelectionConfirmation.is))
     def prodAttributesJS(p: Product) = {
-      val nodeSeq = for (x <- p.createProductLIElemVals) yield <li> {x} </li>
+      val nodeSeq = for (x <- p.createProductLIElemVals) yield <li>{x}</li>
       SetHtml("prodAttributes", nodeSeq)
     }
-    def prodDisplayJS(prod: Product) =  SetHtml("productImg", <img src={prod.imageThumbUrl}/>) & prodAttributesJS(prod) & JsShowId("prodDisplay")
+    def prodDisplayJS(prod: Product) =
+      SetHtml("prodImg", <img src={prod.imageThumbUrl}/>) &
+      prodAttributesJS(prod) &
+      JsShowId("prodDisplay")
     val hideProdDisplayJS =  JsHideId("prodDisplay")
+    // Following 3 values are JavaScript objects to be executed when returning from Ajax call cb to browser to execute on browser
+    // for the 3 events corresponding to the 3 buttons (for normal cases when there are no errors). We need to execute strictly Scala callbacks
+    // here before invoking these JS callbacks. lazy val or def is required here because the value of Session variables changes as we handle events.
     def cancelCbJS = toggleButtonsToRecommendJS & setConfirmJS & hideProdDisplayJS
+    def consumeCbJS = toggleButtonsToRecommendJS & hideProdDisplayJS & setConfirmJS
+    def recommendCbJS = toggleButtonsToConsumeJS & setConfirmJS
 
     def recommend(): JsCmd = {
       def maySelect(): JsCmd =
@@ -57,11 +56,11 @@ class ProductInteraction extends Loggable {
                 } // returns prod normally but if empty, send a notice of error and return empty.
               case util.Failure(ex) => S.error(s"Unable to choose product of category ${TheCategory.is} with error $ex"); Empty
             }
-            confirm = ""
+            TheSelectionConfirmation.set("")
             prod.dmap {
               Noop
             } { p: Product =>
-              product = Full(p)
+              TheProduct.set(Full(p))
               S.clearCurrentNotices // clear error message to make way for normal layout representing normal condition.
               prodDisplayJS(p)
             }
@@ -69,11 +68,11 @@ class ProductInteraction extends Loggable {
           // Error goes to site menu, but we could also send it to a DOM element if we were to specify an additional parameter
         }
 
-      product match {
+      TheProduct.is match {
         case Full(p) => prodDisplayJS(p)
         // ignore consecutive clicks for flow control, ensuring we take only the user's first click as actionable
         // for a series of clicks on button before we have time to disable it
-        case _ => maySelect() & toggleButtonsToConsumeJS & setConfirmJS
+        case _ => maySelect() & recommendCbJS
         // normal processing  (ProductConsume does it the other way around as it plays opposite role as to when it should be active)
       }
     }
@@ -82,15 +81,15 @@ class ProductInteraction extends Loggable {
       def mayConsume(p: Product): JsCmd = {
         Product.consume(p) match {
           case util.Success((userName, count)) =>
-            confirm =s"${p.name} has now been purchased $count time(s), $userName"
-            product = Empty
+            TheSelectionConfirmation.set(s"${p.name} has now been purchased $count time(s), $userName")
+            TheProduct.set(Empty)
             S.clearCurrentNotices // clears error message now that this is good, to get a clean screen.
           case util.Failure(ex) => S.error(s"Unable to sell you product ${p.name} with error '$ex'")
         }
       }
 
-      product match {
-        case Full(p)  => mayConsume(p) & toggleButtonsToRecommendJS & hideProdDisplayJS & setConfirmJS
+      TheProduct.is match {
+        case Full(p)  => mayConsume(p) & consumeCbJS
           // we got notified that we have a product that can be consumed and user expressed interest in consuming.
           // So, try it as a simulation by doing a DB store.
         case _ => Noop // ignore consecutive clicks, ensuring we do not attempt to consume multiple times in a row on a string of clicks from user
@@ -98,12 +97,12 @@ class ProductInteraction extends Loggable {
     }
 
     def cancel(): JsCmd = {
-      confirm = ""
-      product = Empty
+      TheSelectionConfirmation.set("")
+      TheProduct.set(Empty)
       cancelCbJS
     }
 
-    val buttonPairCssSel: CssSel = product match {
+    val buttonPairCssSel: CssSel = TheProduct.is match {
       case Full(p) =>
        "#selectConfirmation" #> s"For social time, we suggest you: ${p.name}"  // assigns RHS text message as value of DOM element selectConfirmation
         "li *" #> p.createProductLIElemVals & // generates many html li items on the fly one per list entry.
@@ -114,7 +113,6 @@ class ProductInteraction extends Loggable {
     val addConsumeImgElem: CssSel = "@consume *+" #> <img src="/images/winesmall.png" alt="consume represented as glass of wine"/>
     val addRecommendImgElem: CssSel = "@recommend *+" #> <img src="/images/recommend.png" alt="recommend represented as question mark"/>
     val addCancelImgElem: CssSel = "@cancel *+" #> <img src="/images/cancel.png" alt="cancel represented as X"/>
-
     "* [onclick]" #> SHtml.ajaxCall(JsRaw("this.value"), { (s: String) =>
        s match {
          case "consume" => consume()
@@ -123,7 +121,7 @@ class ProductInteraction extends Loggable {
          case _ => Noop
        }
     }) & buttonPairCssSel & addCancelImgElem & addRecommendImgElem & addConsumeImgElem &
-    "#confirmMsg *"  #> confirm
+    "#confirmMsg *"  #> TheSelectionConfirmation.is
  }
 
 }
