9c0304788d120ae710360b278d064aef5087d342
Philippe Derome <phderome@gmail.com>
2015-12-25 17:35:45 -0500

    Made Brandy (spirits) image smaller, and with different image types references to it change.
    More importantly, added wine as default category for the buttons for selection (with a style that frames it).

--- src/main/scala/code/snippet/SessionCache.scala
+++ src/main/scala/code/snippet/SessionCache.scala
@@ -9,7 +9,7 @@ import net.liftweb.util.Props
   * Created by philippederome on 2015-12-05.
   */
 object SessionCache {
-  private val defaultCategory = Props.get("product.Category", "wine")
+  val defaultCategory = Props.get("product.Category", "wine")
   private val defaultStore = Props.getInt("store.Id", 1)                  // if 1 it's HWY 401 & Weston.
 
   object theCategory extends SessionVar[String](defaultCategory)
