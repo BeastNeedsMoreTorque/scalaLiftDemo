7a7acb0dbc7ccc62dcac21b7d008d863309c7399
Philippe Derome <phderome@gmail.com>
2015-12-28 16:44:05 -0500

    improved error handling for google maps.

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -2,23 +2,20 @@
 // Global variable, singleton lcboViewer
 // API:
 // <localhost:port>/store/lat/<lat>/lon/<lon> gives closest store from coordinate(lat,lon).
-// Currently supported Store  (see lcboapi.com): id, name, is_dead, distance_in_meters (from specified location), address_line_1, city
+// Currently supported Store  (see lcboapi.com): id, name, is_dead, latitude, longitude, distance_in_meters (from specified location), address_line_1, city
 var lcboViewer = (function() {
-    var currPosition = null;
     var imgSelectStyle = 'imgframe';
     var fetchStore = function(position) {
-        // Show coordinates and store now, later, on a map centered at position
-        if (typeof position != 'undefined') currPosition = position.coords;
+        // Show particulars of nearby store in storeNearby and also show that point in a google map.
         $.ajax({
-            url: '/store/lat/' + currPosition.latitude.toString().replace('.',  ',') + '/lon/' +currPosition.longitude.toString().replace('.', ',')+ '.json', // use commas to help liftweb Req parsing in REST
+            // liftweb interprets periods in special way for suffixes in JSON calls (req parsing), so we escape them by using commas instead.
+            url: '/store/lat/' + position.coords.latitude.toString().replace('.',  ',') + '/lon/' +position.coords.longitude.toString().replace('.', ','),
             type: 'GET',
             success: function(data, status){
-                var coords = position.coords;
-                // #storeNearby: You are XYZ.NN kms from branch <name> located at <address> in <city>
                 var latlon = new google.maps.LatLng(data.latitude, data.longitude)
                 var mapholder = document.getElementById('mapholder')
-                mapholder.style.height = '250px';
-                mapholder.style.width = '500px';
+                mapholder.style.height = '200px';
+                mapholder.style.width = '350px';
 
                 var myOptions = {
                     center:latlon,zoom:14,
@@ -26,10 +23,10 @@ var lcboViewer = (function() {
                     mapTypeControl:false,
                     navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
                 }
-
-                var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+                var map = new google.maps.Map(mapholder, myOptions);
                 var marker = new google.maps.Marker({position:latlon,map:map,title:"Closest Liquor Store!"});
 
+                // #storeNearby: You are XYZ.NN kms from branch <name> located at <address> in <city>
                 var storeDesc = 'You are ' + (data.distance_in_meters/1000.0).toFixed(2) + ' kms from branch '+ data.name +
                     ' located at ' + data.address_line_1 + ' in ' + data.city;
                 $("#storeNearby").html(storeDesc);
@@ -41,10 +38,27 @@ var lcboViewer = (function() {
         });
      };
 
+     var showGeoError = function(error) {
+         switch(error.code) {
+             case error.PERMISSION_DENIED:
+                 $("#storeNearby").html("User denied the request for Geolocation.");
+                 break;
+             case error.POSITION_UNAVAILABLE:
+                 $("#storeNearby").html("User Location information is unavailable.");
+                 break;
+             case error.TIMEOUT:
+                 $("#storeNearby").html("The request to get user location timed out.");
+                 break;
+             case error.UNKNOWN_ERROR:
+                 $("#storeNearby").html("An unknown error occurred.");
+                 break;
+         }
+     };
+
     return {
         fetchStoreFromPosition: function() {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(fetchStore);
+                navigator.geolocation.getCurrentPosition(fetchStore, showGeoError);
             } else  {
                 $("#storeNearby").html("Geolocation is not supported by this browser.");
             }
