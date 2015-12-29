2abc5d89adc866c8718e4934591b2614f1729536
Philippe Derome <phderome@gmail.com>
2015-12-29 09:32:06 -0500

    proper CSS comments using /* */ style rather than incorrect // style.

--- src/main/scala/code/snippet/RadioElements.scala
+++ src/main/scala/code/snippet/RadioElements.scala
@@ -1,11 +1,14 @@
 package code.snippet
 
 import net.liftweb.http.SHtml.{ChoiceHolder, ChoiceItem}
-import net.liftweb.util.Props
 import scala.xml.NodeSeq
 
 /**
   * Created by philippederome on 2015-12-25.
+  * RadioElements function like radio buttons, one element selected among a list but here we erase the radio buttons and
+  * replace normal text labels with image elements. We also handle a frame around the image with a CSS style to suggest selection
+  * is active. This RadioElements also contains a title to provide an intuitive tooltip on the image that acts as a button.
+  * The name of the RadioElements is what we use to receive the input from the client.
   */
 case class RadioElements(name: String, img: NodeSeq) {}
 
