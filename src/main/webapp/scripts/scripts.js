afa03ea1c897872f2f8fb5ae299bdadb9921101c
Philippe Derome <phderome@gmail.com>
2015-12-29 14:27:56 -0500

    ES5 shim for forEach and default downtown Toronto store location if user location unavailable.
    Add organization to sbt. Force login prior to recommend. Remove 0.13.9 from sbt.

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -1,15 +1,36 @@
 "use strict";
+// shim for ECMA-262, http://www.tutorialspoint.com/javascript/array_foreach.htm
+if (!Array.prototype.forEach)
+{
+   Array.prototype.forEach = function(fun /*, thisp*/)
+   {
+      var len = this.length;
+      if (typeof fun != "function")
+      throw new TypeError();
+
+      var thisp = arguments[1];
+      for (var i = 0; i < len; i++)
+      {
+         if (i in this)
+         fun.call(thisp, this[i], i, this);
+      }
+   };
+}
+
 // Global variable, singleton lcboViewer
 // API:
 // <localhost:port>/store/lat/<lat>/lon/<lon> gives closest store from coordinate(lat,lon).
 // Currently supported Store  (see lcboapi.com): id, name, is_dead, latitude, longitude, distance_in_meters (from specified location), address_line_1, city
 var lcboViewer = (function() {
     var imgSelectStyle = 'imgframe';
-    var fetchStore = function(position) {
+    var defaultOntarioLocationLat= 43.647219;  // Bay & Front LCBO address.
+    var defaultOntarioLocationLon= -79.3789987;
+
+    var fetchStore = function(lat, lon, userLocationAvailable) {
         // Show particulars of nearby store in storeNearby and also show that point in a google map.
         $.ajax({
             // liftweb interprets periods in special way for suffixes in JSON calls (req parsing), so we escape them by using commas instead.
-            url: '/store/lat/' + position.coords.latitude.toString().replace('.',  ',') + '/lon/' +position.coords.longitude.toString().replace('.', ','),
+            url: '/store/lat/' + lat.toString().replace('.',  ',') + '/lon/' + lon.toString().replace('.', ','),
             type: 'GET',
             success: function(data, status){
                 var latlon = new google.maps.LatLng(data.latitude, data.longitude)
@@ -19,48 +40,43 @@ var lcboViewer = (function() {
 
                 var myOptions = {
                     center:latlon,zoom:14,
-                    mapTypeId:google.maps.MapTypeId.ROADMAP,
-                    mapTypeControl:false,
-                    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+                    mapTypeId:google.maps.MapTypeId.HYBRID,
+                    mapTypeControl:true
                 }
                 var map = new google.maps.Map(mapholder, myOptions);
-                var marker = new google.maps.Marker({position:latlon,map:map,title:"Closest Liquor Store!"});
-
-                // #storeNearby: You are XYZ.NN kms from branch <name> located at <address> in <city>
-                var storeDesc = 'You are ' + (data.distance_in_meters/1000.0).toFixed(2) + ' kms from branch '+ data.name +
-                    ' located at ' + data.address_line_1 + ' in ' + data.city;
-                $("#storeNearby").html(storeDesc);
-                },
+                var title = 'Downtown Toronto Liquor Store';
+                if (userLocationAvailable) {
+                    // #storeNearby: You are XYZ.NN kms from branch <name> located at <address> in <city>
+                    var storeDesc = 'You are ' + (data.distance_in_meters/1000.0).toFixed(2) + ' kms from branch '+ data.name +
+                        ' located at ' + data.address_line_1 + ' in ' + data.city;
+                    $("#storeNearby").html(storeDesc);
+                    title = "Closest Liquor Store!";
+                } else {
+                    $("#storeNearby").html("Downtown Toronto LCBO (user location unavailable)");
+                }
+                var marker = new google.maps.Marker({position:latlon,map:map,title:title});
+            },
             error: function(data, status){
                 console.log("Error Data: " + data + "\nStatus: " + status );
-                alert("Unable to locate nearest store from your location" );
+                alert("Unable to locate nearest store from specified location" );
             }
         });
      };
+    var fetchStoreNearUser = function(position) {
+        fetchStore(position.coords.latitude, position.coords.longitude, true);
+     };
 
      var showGeoError = function(error) {
-         switch(error.code) {
-             case error.PERMISSION_DENIED:
-                 $("#storeNearby").html("User denied the request for Geolocation.");
-                 break;
-             case error.POSITION_UNAVAILABLE:
-                 $("#storeNearby").html("User Location information is unavailable.");
-                 break;
-             case error.TIMEOUT:
-                 $("#storeNearby").html("The request to get user location timed out.");
-                 break;
-             case error.UNKNOWN_ERROR:
-                 $("#storeNearby").html("An unknown error occurred.");
-                 break;
-         }
+         fetchStore(defaultOntarioLocationLat, defaultOntarioLocationLon, false);
      };
 
     return {
         fetchStoreFromPosition: function() {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(fetchStore, showGeoError);
+                navigator.geolocation.getCurrentPosition(fetchStoreNearUser, showGeoError);
             } else  {
                 $("#storeNearby").html("Geolocation is not supported by this browser.");
+                fetchStore(defaultOntarioLocationLat, defaultOntarioLocationLon, false);
             }
         },
 
