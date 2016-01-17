51347d16dcf849711c2da8e9554f2d1274df7582
Philippe Derome <phderome@gmail.com>
2016-01-17 17:59:19 -0500

    cache store products and improve threading with newer TrieMap Scala collections.
    Touched up JS code a little.
    
    Need to do batch inserts/updates to improve scalability. Also need to avoid select product from DB on fetchSynched if we have it in cache.

--- src/main/scala/code/model/StoreProduct.scala
+++ src/main/scala/code/model/StoreProduct.scala
@@ -1,8 +1,12 @@
 package code.model
 
+import code.model.MainSchema._
+import net.liftweb.db.DB
 import net.liftweb.record.field.LongField
 import net.liftweb.record.{Record, MetaRecord}
+import net.liftweb.squerylrecord.RecordTypeMode._
 import net.liftweb.squerylrecord.KeyedRecord
+import net.liftweb.util.DefaultConnectionIdentifier
 import org.squeryl.annotations.Column
 
 class StoreProduct private() extends Record[StoreProduct] with KeyedRecord[Long] with CreatedUpdated[StoreProduct] {
@@ -17,7 +21,25 @@ class StoreProduct private() extends Record[StoreProduct] with KeyedRecord[Long]
   val productid = new LongField(this)
   val inventory = new LongField(this)
 }
-object StoreProduct extends StoreProduct with MetaRecord[StoreProduct] {}
+
+object StoreProduct extends StoreProduct with MetaRecord[StoreProduct] {
+
+  def insertIfNone(storeId: Long, prodId: Long, inventory: Long) = {
+    DB.use(DefaultConnectionIdentifier) { connection =>
+      val rec = storeProducts.
+        where( (sp: StoreProduct) => sp.storeid === storeId and sp.productid === prodId ).
+        forUpdate.headOption // Load from DB if available, else create it Squeryl very friendly DSL syntax!
+      rec.fold  {
+        val q = createRecord.storeid(storeId).productid(prodId).inventory(inventory)
+        q.save
+        q
+      } { identity // don't try to update for now
+      }
+    }
+  }
+
+
+}
 
 
 
