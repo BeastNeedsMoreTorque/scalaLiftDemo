e91a77579c9392373e800e7051e88caf44f79bae
Philippe Derome <phderome@gmail.com>
2016-02-06 11:09:55 -0500

    Add event handler to select user store. Fixed a store bug for identifying that closest store.

--- src/main/webapp/scripts/lcboviewer.js
+++ src/main/webapp/scripts/lcboviewer.js
@@ -5,26 +5,26 @@
 // <localhost:port>/store/lat/<lat>/lon/<lon> gives closest store from coordinate(lat,lon).
 // Currently supported Store  (see lcboapi.com): id, name, is_dead, latitude, longitude, distance_in_meters (from specified location), address_line_1, city
 var lcboViewer = (function() {
-    var defaultOntarioLocationLat= 43.647219;  // Bay & Front LCBO address.
-    var defaultOntarioLocationLon= -79.3789987;
-
-    var mapCanvas = document.getElementById('map-canvas')
+    var defaultOntarioLocation = new google.maps.LatLng(43.647219, -79.3789987); // Bay & Front LCBO address.
+    var mapCanvas = document.getElementById('map-canvas');
     var map;
+    var userMarker;
     var closestStoreName = 'Unknown Store';
-    var fetchStore = function(lat, lng, userLocationAvailable) {
-        var userLatLng = new google.maps.LatLng(lat, lng)
+    var fetchStore = function(userLatLng, userLocationAvailable, addMarker) {
         var myOptions = {
             center:userLatLng,
             zoom:11,
             mapTypeId:google.maps.MapTypeId.HYBRID,
             mapTypeControl:true
         }
-        map = new google.maps.Map(mapCanvas, myOptions);
-        var userMarker = new google.maps.Marker({position:userLatLng,map:map,title:"Current Location",icon:"http://maps.google.com/mapfiles/ms/icons/green-dot.png"});
+        if (addMarker == true) {
+            map = new google.maps.Map(mapCanvas, myOptions);
+            userMarker = new google.maps.Marker({position:userLatLng,map:map,title:"Current Location",icon:"http://maps.google.com/mapfiles/ms/icons/green-dot.png"});
+        }
         // Show particulars of nearby store in storeNearby and also show that point in a google map.
         $.ajax({
             // liftweb interprets periods in special way for suffixes in JSON calls (req parsing), so we escape them by using commas instead.
-            url: '/stores/lat/' + lat.toString().replace('.',  ',') + '/lng/' + lng.toString().replace('.', ','),
+            url: '/stores/lat/' + userLatLng.lat().toString().replace('.',  ',') + '/lng/' + userLatLng.lng().toString().replace('.', ','),
             type: 'GET',
             success: function(data, status){
                 var latlng = new google.maps.LatLng(data.latitude, data.longitude)
@@ -45,8 +45,10 @@ var lcboViewer = (function() {
                     $("#storeNearby").html("Downtown Toronto LCBO (user location unavailable)");
                     $("#storeAttributesTbl").hide();
                 }
-                var closestMarker = new google.maps.Marker({position:latlng,map:map,title:title,icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"});
-                lcboViewer.fetchStores();
+                if (addMarker == true) {
+                    var closestMarker = new google.maps.Marker({position:latlng,map:map,title:title,icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"});
+                    lcboViewer.fetchStores();
+                }
             },
             error: function(data, status){
                 console.log("Error Data: " + data.responseText + "\nStatus: " + status );
@@ -56,11 +58,13 @@ var lcboViewer = (function() {
     };
 
     var fetchStoreNearUser = function(position) {
-        fetchStore(position.coords.latitude, position.coords.longitude, true);
+        fetchStore(new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            true,
+            true);
     };
 
     var showGeoError = function(error) {
-         fetchStore(defaultOntarioLocationLat, defaultOntarioLocationLon, false);
+         fetchStore(defaultOntarioLocation, false, true);
     };
 
     return {
@@ -77,6 +81,9 @@ var lcboViewer = (function() {
                             if (element.name != closestStoreName) {
                                 var latlng = new google.maps.LatLng(element.latitude, element.longitude);
                                 var marker = new google.maps.Marker({position:latlng,map:map});
+                                marker.addListener('click', function(e){
+                                    fetchStore(e.latLng, true, false);
+                                });
                             }
                         }
                         data.forEach(createMarker);
@@ -93,7 +100,7 @@ var lcboViewer = (function() {
                 navigator.geolocation.getCurrentPosition(fetchStoreNearUser, showGeoError);
             } else  {
                 $("#storeNearby").html("Geolocation is not supported by this browser.");
-                fetchStore(defaultOntarioLocationLat, defaultOntarioLocationLon, false);
+                fetchStore(defaultOntarioLocation, false, true);
             }
         }
     }
