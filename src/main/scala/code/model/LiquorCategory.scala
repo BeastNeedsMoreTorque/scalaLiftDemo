c76d51df10cf63b7f5a01167dcd48e7b2bff6675
Philippe Derome <phderome@gmail.com>
2016-02-06 20:39:25 -0500

    more simplification

--- src/main/scala/code/model/LiquorCategory.scala
+++ src/main/scala/code/model/LiquorCategory.scala
@@ -11,11 +11,10 @@ object LiquorCategory {
   private def getMap(propKey: String): Map[String, String] = {
     val json = parse(Props.get(propKey, "") )
     val l = for (elem <- json.children) yield elem.values // contains List of (String, JString(String))
-    l.map(_.asInstanceOf[(String, String)]).toMap[String, String] // could throw if contents that are configured are in wrong format (violating assumption of pairs (String,String)...
+      l.map(_.asInstanceOf[(String, String)]).toMap // could throw if contents that are configured are in wrong format (violating assumption of pairs (String,String)...
   }
 
   val toPrimaryCategory = getMap("product.CategoriesMap")
   val toImg = getMap("product.CategoriesImageMap")
   val categoriesSeq = Props.get("product.Categories", "wine:beer").split(":").toSeq // give wine and beer at a minimum, provides iterable sequence of categories users can select from.
-  // The list coincides with LCBO pattern keys for searches by design, but it could be made independent if needed.
 }
