d23ea2b253c5b96f9030b6fa58f0bfd3655a4e2f
Philippe Derome <phderome@gmail.com>
2016-02-12 23:06:56 -0500

    progress for solution for multiple products to show with JSON usage on button click at start

--- src/main/webapp/scripts/prodselection.js
+++ src/main/webapp/scripts/prodselection.js
@@ -1,20 +1,15 @@
 "use strict";
 
 // Global variable, singleton prodSelection
-
 var prodSelection = (function() {
   return {
-    clear: function() {
-      $("#chosenProds").val(JSON.stringify([]));
-    },
-
-    checkBoxCB: function(data) {
+    currentProds: function(data) {
       var selectedProds = [];
       var checkedElements = $("div.prodContainer").find("input:checked");
       for (var i = 0; i < checkedElements.length; i++) {
         selectedProds.push(checkedElements[i].value);
       }
-      $("#chosenProds").val(JSON.stringify(selectedProds));
+      return JSON.stringify(selectedProds);
     }
   }
 }());
