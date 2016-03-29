d57a667bd3853aab019572883ae6ead777df0e98
Philippe Derome <phderome@gmail.com>
2016-03-29 19:15:53 -0400

    getSerialResult must not look up the cache since we just failed it, just look up the products directly! Code is getting smaller!

--- src/main/scala/code/Rest/DotDecimalString.scala
+++
@@ -1,9 +0,0 @@
-package code.Rest
-
-/**
-  * Created by philippederome on 2015-12-20.
-  */
-  object DotDecimalString {  // extractor for below
-    def apply(s: String): String = s
-    def unapply(s: String): Option[String] = if (s.contains(',')) Some(s.replace(',', '.'))  else Some(s + ".0")
-  }
