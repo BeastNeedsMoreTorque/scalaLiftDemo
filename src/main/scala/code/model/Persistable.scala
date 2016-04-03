ae5b6d368becf0854565088f949a298b0169fe80
Philippe Derome <phderome@gmail.com>
2016-04-03 17:37:04 -0400

    refactored insert+update that were NOT DRY.

--- src/main/scala/code/model/Persistable.scala
+++ src/main/scala/code/model/Persistable.scala
@@ -32,53 +32,44 @@ trait Persistable[T <: Persistable[T]] extends Loader[T] with ItemStateGrouper w
     insert(insertItems)
   }
 
-  private def batchSize: Int = Props.getInt("DBWrite.BatchSize", 1024)
+  private val batchSize: Int = Props.getInt("DBWrite.BatchSize", 1024)
+  // following cannot be val because of table() usage and timing and need for underlying transaction, connection, etc.
+  private def forceUpdater: (Iterable[T]) => Unit = table().forceUpdate _  // @see http://squeryl.org/occ.html. Regular call as update throws because of possibility of multiple updates on same record.
+  private def insertBatch: (Iterable[T]) => Unit = table().insert _
 
-  // @see http://squeryl.org/occ.html. We can afford to be less strict in our data preparation/validation than for the insert.
-  private def update(items: Iterable[T]) = {
-    items.grouped(batchSize).
-      foreach { batchedItems =>
-        try {
-          table().forceUpdate(batchedItems) // @see http://squeryl.org/occ.html. Regular call as update throws because of possibility of multiple updates on same record.
-          val itemsWithPK = from(table())(item => where( item.idField in items.map(_.pKey)) select(item))
-          cache() ++= itemsWithPK.map{x => x.pKey -> x } (collection.breakOut)
-        } catch {
-          case se: SQLException =>
-            logger.error(s"SQLException $batchedItems")
-            logger.error("Code: " + se.getErrorCode)
-            logger.error("SqlState: " + se.getSQLState)
-            logger.error("Error Message: " + se.getMessage)
-            logger.error("NextException:" + se.getNextException)
-          // intentionally skip other errors and let it go higher up.
-        }
-      }
+  private def batchTransactor(items: Iterable[T], transactor: (Iterable[T]) => Unit) = {
+    def loadToCacheLastTransaction(items: Iterable[T]) = {
+      val itemsWithPK = from(table())(item => where( item.idField in items.map(_.pKey)) select(item))
+      cache() ++= itemsWithPK.map{x => x.pKey -> x } (collection.breakOut)
+    }
+
+    try {
+      transactor(items)
+      loadToCacheLastTransaction(items)
+    } catch {
+      case se: SQLException =>
+        logger.error(s"SQLException $items")
+        logger.error("Code: " + se.getErrorCode)
+        logger.error("SqlState: " + se.getSQLState)
+        logger.error("Error Message: " + se.getMessage)
+        logger.error("NextException:" + se.getNextException)
+      // intentionally skip other errors and let it go higher up.
+    }
   }
+
+  // We can afford to be less strict in our data preparation/validation than for the insert.
+  private def update(items: Iterable[T]) =
+    items.grouped(batchSize).
+      foreach { batchTransactor( _ , forceUpdater) }
 
   private def insert( items: IndexedSeq[T]) = {
     // Do special handling to filter out duplicate keys, which would throw.
-    def insertBatch(items: Iterable[T]): Unit = {
-      // insert them
-      try {  // getNextException in catch is why we want to try catch here.
-        // the DB could fail for PK or whatever other reason.
-        table().insert(items) // refresh them with PKs assigned by DB server.
-        val filteredProdsWithPKs = from(table())(item => where( item.lcboId in items.map(_.lcboId)) select(item))
-        cacheNewItems(filteredProdsWithPKs)
-      } catch {
-        case se: SQLException =>
-          logger.error(s"SQLException $items")
-          logger.error("Code: " + se.getErrorCode)
-          logger.error("SqlState: " + se.getSQLState)
-          logger.error("Error Message: " + se.getMessage)
-          logger.error("NextException:" + se.getNextException)
-        // intentionally skip other serious errors, and let them bubble up.
-      }
-    }
     // first evaluate against cache (assumed in synch with DB) what's genuinely new.
     val LcboIDs = cache().map{ case (id, p) => p.lcboId}.toSet // evaluate once
     // you never know... Our input could have the same product twice in the collection with the same lcbo_id and we have unique index in DB against that.
     items.
         filterNot { p => LcboIDs.contains(p.lcboId) }.  // prevent duplicate primary key for our current data in DB (considering LCBO ID as alternate primary key)
         groupBy {_.lcboId}.map { case (k,item) => item.last }.  // remove duplicates from within our own input, selecting last representative among dupes!
-        grouped(batchSize).foreach { insertBatch } // break it down in reasonable size transactions, and then serialize the work.
+        grouped(batchSize).foreach { batchTransactor( _ , insertBatch) } // break it down in reasonable size transactions, and then serialize the work.
   }
 }
