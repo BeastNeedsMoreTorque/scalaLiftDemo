f2fafdb080592723f802b8a821fb8efe8bdef07e
Philippe Derome <phderome@gmail.com>
2016-04-03 17:09:14 -0400

    improved Persistable (no var) and Inventory (functional composition for fun).

--- src/main/scala/code/model/Persistable.scala
+++ src/main/scala/code/model/Persistable.scala
@@ -7,7 +7,6 @@ import net.liftweb.util.Props
 import net.liftweb.common.Loggable
 import net.liftweb.squerylrecord.KeyedRecord
 import net.liftweb.squerylrecord.RecordTypeMode._
-import org.squeryl.Query
 
 /**
   * Created by philippederome on 2016-03-17.
@@ -35,47 +34,35 @@ trait Persistable[T <: Persistable[T]] extends Loader[T] with ItemStateGrouper w
 
   private def batchSize: Int = Props.getInt("DBWrite.BatchSize", 1024)
 
-  // @see http://squeryl.org/occ.html
+  // @see http://squeryl.org/occ.html. We can afford to be less strict in our data preparation/validation than for the insert.
   private def update(items: Iterable[T]) = {
-    val t = table()
-
     items.grouped(batchSize).
-      foreach { subItems =>
-        var itemsWithPK: Query[T] = null
-        val ids = items.map(_.pKey)
+      foreach { batchedItems =>
         try {
-          t.forceUpdate(subItems) // @see http://squeryl.org/occ.html.
-          itemsWithPK = from(t)(p => where( p.idField in ids) select(p))
+          table().forceUpdate(batchedItems) // @see http://squeryl.org/occ.html. Regular call as update throws because of possibility of multiple updates on same record.
+          val itemsWithPK = from(table())(item => where( item.idField in items.map(_.pKey)) select(item))
+          cache() ++= itemsWithPK.map{x => x.pKey -> x } (collection.breakOut)
         } catch {
           case se: SQLException =>
-            logger.error(s"SQLException $subItems")
+            logger.error(s"SQLException $batchedItems")
             logger.error("Code: " + se.getErrorCode)
             logger.error("SqlState: " + se.getSQLState)
             logger.error("Error Message: " + se.getMessage)
             logger.error("NextException:" + se.getNextException)
           // intentionally skip other errors and let it go higher up.
         }
-
-        // regular call as update throws.
-        // We don't care if two threads attempt to update the same product (from two distinct stores and one is a bit more stale than the other)
-        // However, there are other situations where we might well care.
-        if (itemsWithPK ne null) cache() ++= itemsWithPK.map{x => x.pKey -> x } (collection.breakOut)  // refresh from the database select not from data we sent down.
       }
   }
 
   private def insert( items: IndexedSeq[T]) = {
-    val t = table()
-
     // Do special handling to filter out duplicate keys, which would throw.
     def insertBatch(items: Iterable[T]): Unit = {
       // insert them
-      var filteredProdsWithPKs: Query[T] = null
-      val ids = items.map(_.lcboId)
       try {  // getNextException in catch is why we want to try catch here.
         // the DB could fail for PK or whatever other reason.
-        // we require transaction for insert and following refresh select.
-        t.insert(items) // refresh them with PKs assigned by DB server.
-        filteredProdsWithPKs = from(t)(p => where( p.lcboId in ids) select(p))
+        table().insert(items) // refresh them with PKs assigned by DB server.
+        val filteredProdsWithPKs = from(table())(item => where( item.lcboId in items.map(_.lcboId)) select(item))
+        cacheNewItems(filteredProdsWithPKs)
       } catch {
         case se: SQLException =>
           logger.error(s"SQLException $items")
@@ -85,15 +72,13 @@ trait Persistable[T <: Persistable[T]] extends Loader[T] with ItemStateGrouper w
           logger.error("NextException:" + se.getNextException)
         // intentionally skip other serious errors, and let them bubble up.
       }
-      if (filteredProdsWithPKs ne null) cacheNewItems(filteredProdsWithPKs)
     }
     // first evaluate against cache (assumed in synch with DB) what's genuinely new.
     val LcboIDs = cache().map{ case (id, p) => p.lcboId}.toSet // evaluate once
-    val filteredForRI = items.filterNot { p => LcboIDs.contains(p.lcboId) }
     // you never know... Our input could have the same product twice in the collection with the same lcbo_id and we have unique index in DB against that.
-    val filteredForUnique = filteredForRI.groupBy {_.lcboId}.
-      map { case (k,v) => v.last }
-    // break it down and then serialize the work.
-    filteredForUnique.grouped(batchSize).foreach { insertBatch }
+    items.
+        filterNot { p => LcboIDs.contains(p.lcboId) }.  // prevent duplicate primary key for our current data in DB (considering LCBO ID as alternate primary key)
+        groupBy {_.lcboId}.map { case (k,item) => item.last }.  // remove duplicates from within our own input, selecting last representative among dupes!
+        grouped(batchSize).foreach { insertBatch } // break it down in reasonable size transactions, and then serialize the work.
   }
 }
