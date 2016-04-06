73f2d0347bb10a44dff26225abfed0806cd54db2
Philippe Derome <phderome@gmail.com>
2016-04-05 21:06:37 -0400

    implicit class cannot have an object in same scope with same name

--- src/main/scala/code/model/LCBO_ID.scala
+++ src/main/scala/code/model/LCBO_ID.scala
@@ -4,29 +4,21 @@ import scala.language.implicitConversions
 /**
   * Created by philippederome on 2016-04-04.
   *
-  * @see Value Class, Extension Methods, Correctness (Meter example fitting what we do ) http://docs.scala-lang.org/overviews/core/value-classes.html
-  *      commentary effectively states that JVM limitations force allocations of value classes, most likely when using containers such as they keys
-  *      of my TrieMaps. But, it points the way for more efficiency down the line in the future.
+  * @see Implicit Class http://docs.scala-lang.org/overviews/core/implicit-classes.html
   */
 
 object GlobalLCBO_IDs {
-
   // distinguish two usages of Long types to avoid confusing our primary key with the LCBO's primary key.
   implicit class LCBO_ID(val underlying: Long) extends AnyVal {
     // original toString won't do.
     override def toString = underlying.toString // don't want to send into DOM some TYPE wrapper around the number
   }
-
-  object LCBO_ID {
-    implicit def toLong(id: LCBO_ID): Long = id.underlying
-  }
+  implicit def LCBO_IDtoLong(id: LCBO_ID): Long = id.underlying
 
   implicit class P_KEY(val underlying: Long) extends AnyVal {
     override def toString = underlying.toString // don't want to send into DOM some TYPE wrapper around the number
   }
+  implicit def P_KEYtoLong(id: P_KEY): Long = id.underlying
 
-  object P_KEY {
-    implicit def toLong(id: P_KEY): Long = id.underlying
-  }
 
 }
