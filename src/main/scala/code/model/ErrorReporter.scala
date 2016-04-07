5244ab928c4fa5e9018fe1bb48717817ff211e27
Philippe Derome <phderome@gmail.com>
2016-04-07 07:00:55 -0400

    more refactoring, got rid of equals

--- src/main/scala/code/model/ErrorReporter.scala
+++ src/main/scala/code/model/ErrorReporter.scala
@@ -7,29 +7,23 @@ import net.liftweb.common.{Box, Empty, Loggable}
   * Created by philippederome on 2016-04-03.
   */
 trait ErrorReporter extends Loggable {
-  // returns true on success, false on failure
+  // returns true on success, false on failure, log as side effect
   def checkUnitErrors(box: Box[Unit],
                       contextErr: (String, String) => String): Boolean =
     box match {
       case net.liftweb.common.Failure(m, ex, _) =>
-        val s: String = contextErr(m, ex.toString)
-        logger.error(s)
-        false
+        logger.error(contextErr(m, ex.toString)); false
       case _ => true
     }
 
-  // returns true on success, false on failure
+  // log on failure, true on success
   def checkErrors(box: Box[Iterable[Any]],
                   fullContextErr: (String, String) => String,
                   simpleErr: String) : Boolean =
     box match {
       case net.liftweb.common.Failure(m, ex, _) =>
-        val s: String = fullContextErr(m, ex.toString)
-        logger.error(s)
-        false
-      case Empty =>
-        logger.error(simpleErr)
-        false
+        logger.error(fullContextErr(m, ex.toString)); false
+      case Empty => logger.error(simpleErr); false
       case _ => true
     }
 
