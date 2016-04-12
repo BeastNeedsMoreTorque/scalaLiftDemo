ddc1b2da5dd224ea40705fdf3d9937e7d61db362
Philippe Derome <phderome@gmail.com>
2016-04-11 22:05:38 -0400

    2nd unit test and found a bug!

--- src/main/scala/code/model/ErrorReporter.scala
+++ src/main/scala/code/model/ErrorReporter.scala
@@ -1,7 +1,7 @@
 package code.model
 
 import scala.collection.Iterable
-import net.liftweb.common.{Box, Empty, Loggable}
+import net.liftweb.common.{Box, Empty, Full, Loggable}
 
 /**
   * Created by philippederome on 2016-04-03.
@@ -16,14 +16,14 @@ trait ErrorReporter extends Loggable {
       case _ => true
     }
 
-  // log on failure, true on success
+  // log on failure, true on success. Client expects that the box will contain at least one item and wants an error if there is 0 item.
   def checkErrors(box: Box[Iterable[Any]],
                   fullContextErr: (String, String) => String,
                   simpleErr: String) : Boolean =
     box match {
       case net.liftweb.common.Failure(m, ex, _) =>
         logger.error(fullContextErr(m, ex.toString)); false
-      case Empty => logger.error(simpleErr); false
+      case Full(Nil) => logger.error(simpleErr); false  // This is not None or Empty, just a normal result but empty Iterable.
       case _ => true
     }
 
