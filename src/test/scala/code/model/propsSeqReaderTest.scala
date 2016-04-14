6b8bc09f0ef497e9f2ff269a0a329ec2b3783e7b
Philippe Derome <phderome@gmail.com>
2016-04-14 08:43:45 -0400

    fixed unit test premature commit

--- src/test/scala/code/model/propsSeqReaderTest.scala
+++ src/test/scala/code/model/propsSeqReaderTest.scala
@@ -6,18 +6,17 @@ import org.scalatest.{FlatSpec,ShouldMatchers}
   * Created by philippederome on 2016-04-11. It shows I prefer coding to testing... But code had to stabilize first too.
   */
 class propsSeqReaderTest extends FlatSpec with ShouldMatchers {
-  self: ConfigPairsRepo => // cake pattern!
 
   var masterKey = "product.shortCategories"
   var keyVals = Seq(("wine","Wine"),("spirits","Spirits"),("beer","Beer"))
   "getSeq" should "return sequence of categories" in {
-    getSeq(masterKey) should equal(keyVals)
+    ConfigPairsRepo.ConfigPairsRepoDefaultImpl.getSeq(masterKey) should equal(keyVals)
   }
 
   masterKey = "invalidKey"
   keyVals = Seq()
   "getSeq" should "return empty sequence" in {
-    getSeq(masterKey) should equal(keyVals)
+    ConfigPairsRepo.ConfigPairsRepoDefaultImpl.getSeq(masterKey) should equal(keyVals)
   }
 }
 
