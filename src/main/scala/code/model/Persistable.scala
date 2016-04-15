3a27d6b63ddcf2aee6415cdf8d0a9e907946b6c8
Philippe Derome <phderome@gmail.com>
2016-04-15 07:14:47 -0400

    touched up Persistable to make it more intuitive

--- src/main/scala/code/model/Persistable.scala
+++ src/main/scala/code/model/Persistable.scala
@@ -7,7 +7,7 @@ import net.liftweb.squerylrecord.RecordTypeMode._
 
 /**
   * Created by philippederome on 2016-03-17. Unable to apply cake pattern here and prevent Store and Product to inherit from this,
-  * so mitigate with access control on methods.
+  * so mitigate with access control on methods, one method is protected.
   */
 trait Persistable[T <: Persistable[T]] extends Loader[T] with KeyedRecord[Long] with ORMExecutor {
   self: T =>
@@ -19,27 +19,20 @@ trait Persistable[T <: Persistable[T]] extends Loader[T] with KeyedRecord[Long] 
   }
 
   private val batchSize: Int = Props.getInt("DBWrite.BatchSize", 1024)
-  // following cannot be val because of table() usage and timing and need for underlying transaction, connection, etc.
-  private def forceUpdater: (Iterable[T]) => Unit = table().forceUpdate _  // @see http://squeryl.org/occ.html. Regular call as update throws because of possibility of multiple updates on same record.
-  private def insertBatch: (Iterable[T]) => Unit = table().insert _
-
-  private def loadToCacheLastTransaction(items: Iterable[T]) = {
-    val pkIds = items.map(_.pKey: Long)  // type ascription to integrate with Squeryl below
-    val itemsWithPK = from(table())(item => where(item.idField in pkIds) select(item))
-    cache() ++= itemsWithPK.map{item => item.pKey -> item } (collection.breakOut)
-  }
-
-  private def batchTransactor(items: Iterable[T], transactor: (Iterable[T]) => Unit) = {
-    execute[T](items, transactor)
-    loadToCacheLastTransaction(items)
-  }
 
   // We can afford to be less strict in our data preparation/validation than for the insert.
-  private def update(items: Iterable[T]) =
+  private def update(items: Iterable[T]) = {
+    def ORMUpdater: (Iterable[T]) => Unit = table().forceUpdate _  // @see http://squeryl.org/occ.html. Regular call as update throws because of possibility of multiple updates on same record.
     items.grouped(batchSize).
-      foreach { batchTransactor( _ , forceUpdater) }
+      foreach {
+        batchTransactor(_, ORMUpdater)
+      }
+  }
 
   private def insert( items: IndexedSeq[T]) = {
+    // following cannot be val because of table() usage and timing and need for underlying transaction, connection, etc.
+    def ORMInserter: (Iterable[T]) => Unit = table().insert _
+
     // Do special handling to filter out duplicate keys, which would throw.
     val LcboIDs = from(table())(elt => select(elt.lcboId)).toSet // alas trust the database and not the cache, some other client could insert in database
     // (in fact, a surprising error occurred when trusting cache! Possibly a very subtle bug)
@@ -48,6 +41,16 @@ trait Persistable[T <: Persistable[T]] extends Loader[T] with KeyedRecord[Long] 
     items.
       filterNot { p => LcboIDs.contains(p.lcboId) }.  // prevent duplicate primary key for our current data in DB (considering LCBO ID as alternate primary key)
       groupBy {_.lcboId}.map { case (_, iseq) => iseq(0) }.  // remove duplicate lcboid keys from within our own input, selecting first representative among dupes!
-      grouped(batchSize).foreach { batchTransactor( _ , insertBatch) } // break it down in reasonable size transactions, and then serialize the work.
+      grouped(batchSize).foreach { batchTransactor( _ , ORMInserter) } // break it down in reasonable size transactions, and then serialize the work.
+  }
+
+  private def batchTransactor(items: Iterable[T], ORMTransactor: (Iterable[T]) => Unit) = {
+    def loadToCacheLastTransaction(items: Iterable[T]) = {
+      val pkIds = items.map(_.pKey: Long)  // type ascription to integrate with Squeryl below
+      val itemsWithPK = from(table())(item => where(item.idField in pkIds) select(item))
+      cache() ++= itemsWithPK.map{item => item.pKey -> item } (collection.breakOut)
+    }
+    execute[T](items, ORMTransactor)
+    loadToCacheLastTransaction(items)
   }
 }
