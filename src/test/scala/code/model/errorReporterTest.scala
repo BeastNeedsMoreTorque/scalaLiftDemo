83f122ec82a2570e5f0396bf5d4f1cda7e640b90
Philippe Derome <phderome@gmail.com>
2016-04-30 16:59:34 -0400

    improved error test cases

--- src/test/scala/code/model/errorReporterTest.scala
+++ src/test/scala/code/model/errorReporterTest.scala
@@ -1,8 +1,11 @@
 package code.model
 
+import java.sql.SQLException
+
 import code.UnitTest
 import net.liftweb.util.Helpers.tryo
 import net.liftweb.common.Box
+import net.liftweb.json.MappingException
 
 /**
   * Created by philippederome on 2016-04-11. Look down for "monad" at the end to understand Box.
@@ -18,14 +21,23 @@ class errorReporterTest extends UnitTest {
   def unitBox(body: => Unit): Box[Unit] = tryo(body)
   def intsBox(body: => Iterable[Int]): Box[Iterable[Int]] = tryo(body)
 
+
   behavior of "checkUnitErrors"
-  it should "return error string including some nefarious java.lang.IllegalArgumentException substring" in {
-    def unit = {
-      throw new IllegalArgumentException("Dropped the BOMB or some pleasant/useful IT/Product context") // you poor chap!
-    }
-    instance.checkUnitErrors(unitBox(unit), errorFormatter) should include("Full(java.lang.IllegalArgumentException:") // and trace stuff along the way
+  // Please note the absence of try catch blocks.
+  private def testExceptions[T >: Exception](test: (T, String) => Unit) = {
+    val tests = Table(
+      ("ex: Exception", "msg: String"), // The following are NOT The Magnificent Seven nor The Fab Four.
+      (new IllegalArgumentException("Consider Partial Functions as with Actors"), "as with Actors...Full(java.lang.IllegalArgumentException:"),
+      (new NullPointerException("Consider using Scala and Options"), "Scala and Options...Full(java.lang.NullPointerException:"),
+      (new ArithmeticException("was this a bad case of DIV BY 0?"), "DIV BY 0?...Full(java.lang.ArithmeticException"),
+      (new SQLException("Is the developer or DBA forgetful? Brittle data validation? access control or referential integrity issue?"), "control or referential integrity issue?...Full(java.sql.SQLException"),
+      (new MappingException("Consider extractOpt?"), "extractOpt?...Full(net.liftweb.json.MappingException"))
+    forAll(tests)(test)
   }
-  
+
+  private def testCaptureException(ex: Exception, msg: String) = instance.checkUnitErrors(unitBox({throw ex}), errorFormatter) should include(msg)
+  it should "work for miscellaneous thrown exceptions" in testExceptions(testCaptureException)
+
   it should "return empty string on harmless normal box" in {
     def unit = {
       val nearZero = 0.0001
@@ -34,38 +46,38 @@ class errorReporterTest extends UnitTest {
     instance.checkUnitErrors(unitBox(unit), errorFormatter) shouldBe empty
   }
 
-  it should "return a specific error containing ArithmeticException with something about zero on DIV BY 0" in {
-    def unit =  { val x = 5/0 }// kaboom! Disjunctor to the rescue (a Lift one not a Scalaz one)!
-    instance.checkUnitErrors(unitBox(unit), errorFormatter) should include("Full(java.lang.ArithmeticException: / by zero)")
-  }
 
   behavior of "checkErrors"
+
   it should "return empty on normal Iterable[Int]" in {
     def ints = {
       val nearZeroInt = 1
       val x = 5/nearZeroInt
       Seq(x, 1,2,3)
     }
-    val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
-    val errCheck = instance.checkErrors(intsBox(ints), errorFormatter, appContextError)
+    val appContxtError = "YOUR Integer COLLECTION IS EMPTY BECAUSE of too tight of a filter perhaps? TERRIBLY SORRY! ErrNo:654321, Sev:16" // this is hobby project, not corporate, professional, I have license for irony.
+    val errCheck = instance.checkErrors(intsBox(ints), errorFormatter, appContxtError)
     errCheck shouldBe empty
   }
 
-  it should "return a specific error with ArithmeticException and not some appContextError" in {
+  it should "return a specific error with ArithmeticException and not some app contxt error" in {
     def ints =  {
       val x: Int = 5/0
       Seq(x, 1,2,3)
     }
-    val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
-    instance.checkErrors(intsBox(ints), errorFormatter, appContextError) should equal("/ by zero...Full(java.lang.ArithmeticException: / by zero)")
+    val appContxtError = "YOUR Integer COLLECTION IS EMPTY BECAUSE of too tight of a filter perhaps? TERRIBLY SORRY! ErrNo:123456, Sev: 18" // this is hobby project, not corporate, professional, I have license for irony.
+    instance.checkErrors(intsBox(ints), errorFormatter, appContxtError) should equal("/ by zero...Full(java.lang.ArithmeticException: / by zero)") // Ouch!!
   }
 
   it should "return app context message on unexpected empty collection" in {
-    val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!"
-    instance.checkErrors(intsBox{ IndexedSeq()}, errorFormatter, appContextError) should equal(appContextError)
+    val appContxtError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY! ErrNo:911, Sev: 4S"
+    instance.checkErrors(intsBox{ IndexedSeq()}, errorFormatter, appContxtError) should equal(appContxtError)
   }
 
+
+
   behavior of "Box monad"
+
   it should "return numbers as sequence of size 1 on normal Iterable[Int] after successful checkErrors" in {
     def ints = {
       val nearZeroInt = 1
@@ -73,7 +85,7 @@ class errorReporterTest extends UnitTest {
       Seq(x)
     }
     val monad = intsBox(ints)
-    val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
+    val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY! ErrNo:223344, Sev: 1024" // this is hobby project, not corporate, professional, I have license for irony.
     val errCheck = instance.checkErrors(monad, errorFormatter, appContextError)
     assert(errCheck.isEmpty)
     val numbers = for (x <- monad) yield x  // Monad usage (flatMap)
