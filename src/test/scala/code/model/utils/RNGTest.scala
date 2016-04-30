d13f1c12408d8d9f2b2f9d0d2320001d7ac90016
Philippe Derome <phderome@gmail.com>
2016-04-29 20:57:29 -0400

    improving unit testing using ScalaTest a bit better

--- src/test/scala/code/model/utils/RNGTest.scala
+++ src/test/scala/code/model/utils/RNGTest.scala
@@ -16,6 +16,8 @@ class RNGTest extends UnitTest {
   val sampleSize = 100
   val samples = sequence(List.fill(sampleSize)(nonNegativeInt))(r)
 
+  behavior of "checkNonNegative"
+
   "checkNonNegative" should s"return only actual negatives on 'large' input (size $sampleSize) when setting Simple($seed0)" in {
     val allNonNegatives = samples._1.forall(_ >= 0)
     allNonNegatives shouldBe true
@@ -26,27 +28,53 @@ class RNGTest extends UnitTest {
     sampleResSize shouldBe sampleSize
   }
 
+  behavior of "checkShuffle"
+
   val shuffleOffset = 100
   val shuffleRange = (0 to 9).map( _ + shuffleOffset)
   val seed1 = 20
   "checkShuffle" should s"predict correctly permutation of $shuffleRange when setting Simple($seed1)" in {
     val (shuffled, _) = shuffle(shuffleRange)(Simple(seed1))
-    val exp_shuffled = List(107, 106, 108, 101, 105, 104, 103, 102, 109, 100)
+    val exp_shuffled = List(104, 105, 107, 101, 108, 106, 103, 109, 102, 100)
     shuffled should equal(exp_shuffled)
   }
 
   val seed2 = 10
   it should s"predict correctly permutation of $shuffleRange when setting with other seed Simple($seed2)" in {
     val (shuffled, _) = shuffle(shuffleRange)(Simple(seed2))
-    val exp_shuffled = List(100, 108, 101, 105, 107, 109, 102, 106, 103, 104)
+    val exp_shuffled = List(102, 109, 100, 107, 105, 101, 108, 103, 106, 104)
     shuffled should equal(exp_shuffled)
   }
+
+  it should s"return same element on permuting 1 element when setting with random seed" in {
+    val elem = Random.nextInt()
+    val seed = Random.nextInt()
+    val (shuffled, _) = shuffle(List(elem))(Simple(seed))
+    val exp_shuffled = List(elem)
+    shuffled should equal(exp_shuffled)
+  }
+
+  it should s"return empty sequence on permuting 0 element when setting with random seed" in {
+    val seed = Random.nextInt()
+    val exp_shuffled = List()
+    val (shuffled, _) = shuffle(exp_shuffled)(Simple(seed))
+    shuffled should equal(exp_shuffled)
+  }
+
+  it should s"return sequence with no duplicates on permuting large sequence with random seed" in {
+    val seed = Random.nextInt()
+    val N = 4000
+    val (shuffled, _) = shuffle(1 to N)(Simple(seed))
+    shuffled should have size N
+  }
+
+  behavior of "checkCollectSample"
 
   val bigSample = 1 to 5000
   val k1 = 5
   val seed3 = 50
   val (selected, newSimple) = collectSample(bigSample, k1)(Simple(seed3))
-  val exp_selected = Seq(3524, 520, 3544, 4675, 3129)
+  val exp_selected = Seq(3129, 4675, 3544, 520, 3524)
   "checkCollectSample" should s"predict pick $k1 distinct elements from 1 to 5000 exactly as ($exp_selected, Simple(239178062524073)) on seed Simple($seed3)" in {
     (selected, newSimple) should equal((exp_selected), Simple(239178062524073L))
   }
