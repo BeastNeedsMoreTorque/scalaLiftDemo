6250a007f29f42a01d324512e5eed361e2343512
Philippe Derome <phderome@gmail.com>
2016-04-30 11:15:09 -0400

    textual changes

--- src/test/scala/code/model/propsSeqReaderTest.scala
+++ src/test/scala/code/model/propsSeqReaderTest.scala
@@ -3,17 +3,17 @@ package code.model
 import code.model.utils.UnitTest
 
 /**
-  * Created by philippederome on 2016-04-11. It shows I prefer coding to testing... But code had to stabilize first too.
+  * Created by philippederome on 2016-04-11.
   * Uses main/resources/test.default.props for Properties content.
   */
 class propsSeqReaderTest extends UnitTest {
 
-  "getSeq" should "return exact sequence of categories (json parsed) keyed by masterKey" in {
+  "getSeq" should "return exact sequence of categories (json parsed) keyed by valid masterKey" in {
     val keyVals = Seq(("wine","Wine"),("spirits","Spirits"),("beer","Beer"))
     ConfigPairsRepo.defaultInstance.getSeq("product.shortCategories") should equal(keyVals)
   }
 
-  "getSeq" should "return empty sequence when masterKey is not found in Props" in {
+  "getSeq" should "return empty sequence when masterKey is invalid" in {
     ConfigPairsRepo.defaultInstance.getSeq("invalidKey") shouldBe empty
   }
 }
