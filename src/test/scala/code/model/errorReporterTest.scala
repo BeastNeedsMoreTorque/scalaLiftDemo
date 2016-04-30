ccb5d38c48dd8c81e13ef995508912f8c3754d81
Philippe Derome <phderome@gmail.com>
2016-04-30 13:57:02 -0400

    fixture

--- src/test/scala/code/model/errorReporterTest.scala
+++ src/test/scala/code/model/errorReporterTest.scala
@@ -15,67 +15,68 @@ class errorReporterTest extends UnitTest {
   val instance = new errorReporterClass
   def errorFormatter( s: String,  suffix: String): String = s"$s...$suffix"
 
+  def unitBox(body: => Unit): Box[Unit] = tryo(body)
+  def intsBox(body: => Iterable[Int]): Box[Iterable[Int]] = tryo(body)
+
   behavior of "checkUnitErrors"
   it should "return error string including some nefarious java.lang.IllegalArgumentException substring" in {
-    val box: Box[Unit] =  tryo {
+    def unit = {
       throw new IllegalArgumentException("Dropped the BOMB or some pleasant/useful IT/Product context") // you poor chap!
     }
-    instance.checkUnitErrors(box, errorFormatter) should include("Full(java.lang.IllegalArgumentException:") // and trace stuff along the way
+    instance.checkUnitErrors(unitBox(unit), errorFormatter) should include("Full(java.lang.IllegalArgumentException:") // and trace stuff along the way
   }
   
   it should "return empty string on harmless normal box" in {
-    val box: Box[Unit] = tryo {
+    def unit = {
       val nearZero = 0.0001
       val x = 5/nearZero // near miss!
     }
-    instance.checkUnitErrors(box, errorFormatter) shouldBe empty
+    instance.checkUnitErrors(unitBox(unit), errorFormatter) shouldBe empty
   }
 
   it should "return a specific error containing ArithmeticException with something about zero on DIV BY 0" in {
-    val box = tryo {
-      val x = 5/0 // kaboom! Disjunctor to the rescue (a Lift one not a Scalaz one)!
-    }
-    instance.checkUnitErrors(box, errorFormatter) should include("Full(java.lang.ArithmeticException: / by zero)")
+    def unit =  { val x = 5/0 }// kaboom! Disjunctor to the rescue (a Lift one not a Scalaz one)!
+    instance.checkUnitErrors(unitBox(unit), errorFormatter) should include("Full(java.lang.ArithmeticException: / by zero)")
   }
 
   behavior of "checkErrors"
   it should "return empty on normal Iterable[Int]" in {
-    val box: Box[Iterable[Int]] = tryo {
+    def ints = {
       val nearZeroInt = 1
       val x = 5/nearZeroInt
       Seq(x, 1,2,3)
     }
     val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
-    val errCheck = instance.checkErrors(box, errorFormatter, appContextError)
+    val errCheck = instance.checkErrors(intsBox(ints), errorFormatter, appContextError)
     errCheck shouldBe empty
   }
 
   it should "return a specific error with ArithmeticException and not some appContextError" in {
-    var box: Box[Iterable[Int]] = tryo {
+    def ints =  {
       val x: Int = 5/0
       Seq(x, 1,2,3)
     }
     val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
-    instance.checkErrors(box, errorFormatter, appContextError) should equal("/ by zero...Full(java.lang.ArithmeticException: / by zero)")
+    instance.checkErrors(intsBox(ints), errorFormatter, appContextError) should equal("/ by zero...Full(java.lang.ArithmeticException: / by zero)")
   }
 
   it should "return app context message on unexpected empty collection" in {
-    val boxContainingEmpty: Box[Iterable[Int]] = tryo { IndexedSeq()}
     val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!"
-    instance.checkErrors(boxContainingEmpty, errorFormatter, appContextError) should equal(appContextError)
+    instance.checkErrors(intsBox{ IndexedSeq()}, errorFormatter, appContextError) should equal(appContextError)
   }
 
   behavior of "Box monad"
   it should "return numbers as sequence of size 1 on normal Iterable[Int] after successful checkErrors" in {
-    val box: Box[Iterable[Int]] = tryo {
+    def ints = {
       val nearZeroInt = 1
       val x = 5/nearZeroInt
       Seq(x)
     }
+    val monad = intsBox(ints)
     val appContextError = "YOUR Integer COLLECTION IS EMPTY BECAUSE OF USER TRAINING ISSUE, TERRIBLY SORRY!" // this is hobby project, not corporate, professional, I have license for irony.
-    val errCheck = instance.checkErrors(box, errorFormatter, appContextError)
-    errCheck shouldBe empty
-    val numbers = for (x <- box) yield x  // Monad usage (flatMap)
+    val errCheck = instance.checkErrors(monad, errorFormatter, appContextError)
+    assert(errCheck.isEmpty)
+    val numbers = for (x <- monad) yield x  // Monad usage (flatMap)
     numbers.map(identity).getOrElse(1 to 10) should have size 1  // not 10!
   }
 }
--- src/test/scala/code/model/utils/RNGTest.scala
+++ src/test/scala/code/model/utils/RNGTest.scala
@@ -1,34 +1,40 @@
 package code.model.utils
 
 import scala.util.Random
-import org.junit.runner.RunWith
 import RNG._
 import State._
 import code.UnitTest
 import code.SlowTest
-
+import scala.language.reflectiveCalls
 
 /**
   * Created by philippederome on 2016-04-28.
   * Unit testing is useful: it helped me identify stack overflow on shuffle for large N and later on rather egregious performance in my original
   * greedy, naive algorithm.
   */
-@RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class RNGTest extends UnitTest {
+
+  // technique normally used to localize side effects to single test cases.
+  trait randomRNG {
+    val seed = Simple(Random.nextInt())
+  }
+
+  trait randomInt {
+    val elem = Random.nextInt()
+  }
 
   behavior of "NonNegative"
   val seed0 = 10
-  val r = Simple(seed0)
+  val r0 = Simple(seed0)
   val sampleSize = 100
-  val (samples, _) = sequence(List.fill(sampleSize)(nonNegativeInt)).run(r)
-  it should s"return only actual negatives on 'large' input (size $sampleSize) when setting Simple($seed0)" in {
+  val (samples, _) = sequence(List.fill(sampleSize)(nonNegativeInt)).run(r0)
+  it should s"return only actual negatives on 'large' input when setting specific Simple" in {
     val allNonNegatives = samples.forall(_ >= 0)
     allNonNegatives shouldBe true
   }
 
   it should s"check that sample size of results has no repeats (same as sampleSize $sampleSize)" in {
-    val sampleResSize = samples.toSet.size
-    sampleResSize shouldBe sampleSize
+    samples.toSet.size shouldBe sampleSize
   }
 
   behavior of "Shuffle"
@@ -37,63 +43,58 @@ class RNGTest extends UnitTest {
   // So the check is more of a solution/function check than a BEHAVIOUR one.
   val shuffleOffset = 100
   val shuffleRange = (0 to 9).map( _ + shuffleOffset)
-  val seed1 = 20
-  it should s"predict correctly permutation of $shuffleRange when setting Simple($seed1)" in {
+  it should s"predict correctly permutation of $shuffleRange when setting specific Simple" in {
+    val seed1 = 20
     val (shuffled, _) = shuffle(shuffleRange).run(Simple(seed1))
-    val exp_shuffled = List(109, 106, 102, 105, 101, 104, 103, 107, 100, 108)
-    shuffled should equal(exp_shuffled)
+    val expected_shuffled = List(109, 106, 102, 105, 101, 104, 103, 107, 100, 108)
+    shuffled should equal(expected_shuffled)
   }
 
-  val seed2 = 10
-  it should s"predict correctly permutation of $shuffleRange when setting with other seed Simple($seed2)" in {
+  it should s"predict correctly permutation of $shuffleRange when setting with other specific Simple" in {
+    val seed2 = 10
     val (shuffled, _) = shuffle(shuffleRange).run(Simple(seed2))
-    val exp_shuffled = List(105, 106, 101, 109, 102, 100, 107, 104, 108, 103)
-    shuffled should equal(exp_shuffled)
+    val expected_shuffled = List(105, 106, 101, 109, 102, 100, 107, 104, 108, 103)
+    shuffled should equal(expected_shuffled)
   }
 
-  it should s"return same element on permuting 1 element when setting with random seed" in {
-    val elem = Random.nextInt()
-    val seed = Random.nextInt()
-    val (shuffled, _) = shuffle(List(elem)).run(Simple(seed))
-    val exp_shuffled = List(elem)
-    shuffled should equal(exp_shuffled)
+  it should s"return same element on permuting 1 element when setting with random seed" in new randomRNG with randomInt {
+    val (shuffled, _) = shuffle(List(elem)).run(seed)
+    val expected_shuffled = List(elem)
+    shuffled should equal(expected_shuffled)
   }
 
-  it should s"return empty sequence on permuting 0 element when setting with random seed" in {
-    val seed = Random.nextInt()
-    val exp_shuffled = List()
-    val (shuffled, _) = shuffle(exp_shuffled).run(Simple(seed))
-    shuffled should equal(exp_shuffled)
+  it should s"return empty sequence on permuting 0 element when setting with random seed" in new randomRNG {
+    val expected_shuffled = List()
+    val (shuffled, _) = shuffle(expected_shuffled).run(seed)
+    shuffled should equal(expected_shuffled)
   }
 
   // This used to do stack overflow at about 2000-3000 items, which was NOT FUN AT ALL!
   // Still about 4-7 times slower than the official Random.shuffle one on 10000 items (mine is 1.0 sec to 1.7 sec compared to 246 ms)
   // Warning: moderately slow.
-  it should s"return sequence with no duplicates on permuting large sequence with random seed" taggedAs(SlowTest) in {
-    val seed = Random.nextInt()
+  it should s"return sequence with no duplicates on permuting large sequence with random seed" taggedAs(SlowTest) in new randomRNG {
     val N = 10000
-    val (shuffled, _) = shuffle(1 to N).run(Simple(seed))
+    val (shuffled, _) = shuffle(1 to N).run(seed)
     shuffled should have size N
   }
 
   behavior of "CollectSample"
-
   val bigSample = 1 to 5000
-  val k1 = 5
-  val seed3 = 50
-  val (selected, newSimple) = collectSample(bigSample, k1).run(Simple(seed3))
-  val exp_selected = Seq(3524, 520, 3544, 4675, 3129)
-  it should s"predict pick $k1 distinct elements from 1 to 5000 exactly as ($exp_selected, Simple(239178062524073)) on seed Simple($seed3)" in {
-    (selected, newSimple) should equal((exp_selected), Simple(239178062524073L))
+  it should s"predict pick 5 distinct elements from 1 to 5000 exactly on specific seed Simple" in {
+    val k1 = 5
+    val seed3 = 50
+    val (selected, newSimple) = collectSample(bigSample, k1).run(Simple(seed3))
+    val expected_shuffled = Seq(3524, 520, 3544, 4675, 3129)
+    (selected, newSimple) should equal(expected_shuffled, Simple(239178062524073L))
   }
 
-  it should "get empty Sequence on choosing 0 item from non empty list with any random input" in {
-    val (seq, _) = collectSample(bigSample, 0).run(Simple(Random.nextInt()))
+  it should "get empty Sequence on choosing 0 item from non empty list with any random input" in new randomRNG {
+    val (seq, _) = collectSample(bigSample, 0).run(seed)
     seq shouldBe empty
   }
 
-  it should "get empty Sequence on choosing 10 items from empty list with any random input" in {
-    val (seq, _) = collectSample(Seq[Int](), 10).run(Simple(Random.nextInt()))
+  it should "get empty Sequence on choosing 10 items from empty list with any random input" in new randomRNG {
+    val (seq, _) = collectSample(Seq[Int](), 10).run(seed)
     seq shouldBe empty
   }
 
