b501a724ea4bdfd5aeedec537d3b59b1cf8fb0de
Philippe Derome <phderome@gmail.com>
2016-05-04 19:34:19 -0400

    improved first trivial test for ProductAdvisor!

--- src/test/scala/code/model/ProductAdvisorComponentImplTest.scala
+++ src/test/scala/code/model/ProductAdvisorComponentImplTest.scala
@@ -7,8 +7,6 @@ import net.liftweb.common.Full
 
 import scala.collection.IndexedSeq
 
-import Product.fetchByStoreCategory
-
 /**
   * Created by philippederome on 2016-05-04.
   */
@@ -18,20 +16,26 @@ class ProductAdvisorComponentImplTest extends UnitTest {
   val instance = new ProductAdvisorComponentImplTest
   val drunkShuffler = instance.agent
 
-
-  object MockNoProduct extends ProductRunner with InventoryService {
-    // Mock it. Could create (or better yet generate randomly with ScalaCheck) a handful of concrete Product instances.
-    override def asyncLoadCache() = {} // intentional Noop here.
+  trait MockInventoryService extends InventoryService {
     override def pKey: P_KEY = P_KEY(1)
     override def lcboId: LCBO_ID = LCBO_ID(1)
+    override val inventoryByProductIdMap: P_KEY => Option[Inventory] = key => None
+    override def getProductsByCategory(lcboCategory: String) = IndexedSeq[IProduct]()
+    override def asyncLoadCache() = {} // intentional Noop here.
+  }
+
+  trait MockProductRunner extends ProductRunner {
+    // Could create (or better yet generate randomly with ScalaCheck) a handful of concrete Product instances.
+
     //Need some reasonable simulation for following. With just a bit more work, we could have something really interesting here.
     override def fetchByStoreCategory(lcboStoreId: Long, category: String, requiredSize: Int): IndexedSeq[IProduct] = IndexedSeq[Product]()
-    override def getProductsByCategory(lcboCategory: String) = IndexedSeq[IProduct]()
-    override val inventoryByProductIdMap: P_KEY => Option[Inventory] = key => None
   }
+
+  object MockProductWithNothingUseful extends MockProductRunner with MockInventoryService {}
 
   behavior of "No input"
   it should s"advise an empty Stream of products when no products of category can be found" in {
-    drunkShuffler.advise(MockNoProduct, "wine", 5, MockNoProduct) should equal(Full(Stream()))
+    drunkShuffler.advise(MockProductWithNothingUseful, "wine", 5, MockProductWithNothingUseful) should equal(Full(Stream()))
   }
+
 }
