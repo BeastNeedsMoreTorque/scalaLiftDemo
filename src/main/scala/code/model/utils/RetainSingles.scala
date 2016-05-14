22a0d5ca3cc67a0c90461572c2f630b88124e84f
Philippe Derome <phderome@gmail.com>
2016-05-14 07:58:11 -0400

    preserve order in RetainSingles.generalRemoveDupes, add comments to README.
    Lose a bit of generality in RetainSingles in favour of Seq.
    
    construct Seq with prepend as opposed to fusing two sequences.

--- src/main/scala/code/model/utils/RetainSingles.scala
+++ src/main/scala/code/model/utils/RetainSingles.scala
@@ -19,12 +19,12 @@ object RetainSingles extends Loggable {
     }
   }
 
-  case class RemoveDupesState[T](keys: Set[String], discarded: Seq[T], retained: Seq[T])
-  def generalRemoveDupes[T <: KeyHolder](items: Iterable[T])(onFailure: Iterable[T] => Unit): Iterable[T] = {
-    val pair = items.foldLeft(RemoveDupesState(Set.empty[String], Seq.empty[T], Seq.empty[T])) { (acc, item) =>
+  case class RemoveDupesState[T](keys: Set[String], discarded: IndexedSeq[T], retained: IndexedSeq[T])
+  def generalRemoveDupes[T <: KeyHolder](items: Seq[T])(onFailure: Seq[T] => Unit): Seq[T] = {
+    val pair = items.reverse.foldLeft(RemoveDupesState(Set.empty[String], IndexedSeq.empty[T], IndexedSeq.empty[T])) { (acc, item) =>
       val k = item.getKey
-      if (acc.keys(k)) RemoveDupesState(keys=acc.keys, discarded=acc.discarded ++ Seq(item), retained=acc.retained)
-      else RemoveDupesState(keys=acc.keys + k, discarded=acc.discarded, retained=acc.retained ++ Seq(item))
+      if (acc.keys(k)) RemoveDupesState(keys=acc.keys, discarded= item +: acc.discarded, retained=acc.retained)
+      else RemoveDupesState(keys=acc.keys + k, discarded=acc.discarded, retained= item +: acc.retained)
     }
     onFailure(pair.discarded)
     pair.retained
@@ -35,18 +35,18 @@ object RetainSingles extends Loggable {
     items.foreach(x => logger.warn(s"discarded key:${x.getKey} item:$x"))
 
   // default is noisy.
-  def removeDupes[T <: KeyHolder](items: Iterable[T]): Iterable[T] =
+  def removeDupes[T <: KeyHolder](items: Seq[T]): Seq[T] =
     generalRemoveDupes(items)(logDiscarded)
 
   // @see http://www.scala-notes.org/2010/06/avoid-structural-types-when-pimping-libraries/
-  class Result[A <: KeyHolder](iter: Iterable[A]) {
+  class Result[A <: KeyHolder](iter: Seq[A]) {
     def removeDupeds = removeDupes(iter)
   }
 
   // Minor use of "pimp/enrich my library" pattern (removeDupeds).
-  implicit def implicitIterToSyntax[A <: KeyHolder](iter: Iterable[A]) = new Result(iter)
+  implicit def implicitIterToSyntax[A <: KeyHolder](iter: Seq[A]) = new Result(iter)
 
   val noop = (a: Any) => ()
-  def removeDupesQuietly[T <: KeyHolder](items: Iterable[T]): Iterable[T] =
+  def removeDupesQuietly[T <: KeyHolder](items: Seq[T]): Seq[T] =
     generalRemoveDupes(items){ noop }
 }
