e5aa478c698ce8e59332f08ac07d50e186b1720a
Phil Derome <phderome@gmail.com>
2016-08-14 13:19:04 -0400

    do not waste resources on Futures, create only one if all we need is one.

--- src/main/scala/code/model/StoreCacheService.scala
+++ src/main/scala/code/model/StoreCacheService.scala
@@ -19,6 +19,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * Created by philippederome on 2016-07-31.
   */
 trait StoreCacheService extends ORMExecutor with Loggable {
+  type StringFormatter = String => String
+
   val productsCache: TrieMap[LCBO_ID, IProduct]
   val inventoryByProductId: TrieMap[P_KEY, Inventory]
   val productsCacheByCategory: TrieMap[String, IndexedSeq[KeyKeeperVals]]
@@ -29,21 +31,21 @@ trait StoreCacheService extends ORMExecutor with Loggable {
 
   // generally has side effect to update database with more up to date content from LCBO's (if different)
   def loadCache(): Unit = {
-    val productsContext: String => String = s => s"Problem loading products into cache with exception error $s"
-    val inventoriesContext: String => String = s => s"Problem loading inventories into cache for '$lcboId' with exception error $s"
-    val fetches =
-      for {p <- Future(fetchProducts(productsContext)) // fetch and then make sure model/Squeryl classes update to DB and their cache synchronously,
-           // so we can use their caches.
-           // similarly for inventories and serialize products then inventories intentionally because of Ref.Integrity (inventory depends on valid product)
-           i <- Future(fetchInventories(inventoriesContext))} yield i
-
+    val productsContext: StringFormatter = s => s"Problem loading products into cache with exception error $s"
+    val inventoriesContext: StringFormatter = s => s"Problem loading inventories into cache for '$lcboId' with exception error $s"
+    val fetchProdsAndInvs: (StringFormatter, StringFormatter) => Unit = (pFormat, iFormat) => {
+      fetchProducts(pFormat)
+      fetchInventories(iFormat)
+      // serialize products then inventories intentionally because of Ref.Integrity (inventory depends on valid product)
+    }
+    val fetches = Future(fetchProdsAndInvs(productsContext, inventoriesContext))
+    logger.info(s"loadCache async launched for $lcboId") // about 15 seconds, likely depends mostly on network/teleco infrastructure
     fetches onComplete {
       case Success(_) => // We've persisted along the way for each LCBO page ( no need to refresh because we do it each time we go to DB)
         logger.debug(s"loadCache async work succeeded for $lcboId")
         if (emptyInventory) logger.warn(s"got no product inventory for storeId $lcboId !") // No provision for retrying.
       case Failure(f) => logger.info(s"loadCache explicitly failed for $lcboId cause ${f.getMessage}")
     }
-    logger.info(s"loadCache async launched for $lcboId") // about 15 seconds, likely depends mostly on network/teleco infrastructure
   }
 
   def addToCaches(items: IndexedSeq[IProduct]): Unit = {
@@ -56,18 +58,18 @@ trait StoreCacheService extends ORMExecutor with Loggable {
       mapValues(_.map(x => x.keys))
   }
 
-  private def fetchProducts(contextErrFormatter: String => String) = {
+  private def fetchProducts(contextErrFormatter: String => String): Unit = {
     val theProducts = fetchByStore(lcboId)
-    val fail = { t: Throwable => errHandler(t, contextErrFormatter) }
-    val success = { items: IndexedSeq[IProduct] =>
+    val logErrorOnFail = { t: Throwable => errHandler(t, contextErrFormatter) }
+    val cacheOnSuccess = { items: IndexedSeq[IProduct] =>
         if (items.isEmpty) logger.error("Problem loading products into cache, none found")
         addToCaches(items)
       }
 
-    theProducts.fold[Unit]( fail, success)
+    theProducts.fold[Unit]( logErrorOnFail, cacheOnSuccess)
   }
 
-  private def fetchInventories(contextErrFormatter: String => String) = {
+  private def fetchInventories(contextErrFormatter: String => String): Unit = {
     def inventoryTableUpdater: (Iterable[Inventory]) => Unit = MainSchema.inventories.update
     def inventoryTableInserter: (Iterable[Inventory]) => Unit = MainSchema.inventories.insert
     // fetch @ LCBO, store to DB and cache into ORM stateful caches, trap/log errors, and if all good, refresh our own store's cache.
