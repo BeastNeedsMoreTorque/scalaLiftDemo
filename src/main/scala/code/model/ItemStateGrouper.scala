53380e42b3c0f847879696ced01d7d963624866d
Phil Derome <phderome@gmail.com>
2016-08-15 20:25:12 -0400

    abstract out ORMExecutor a bit

--- src/main/scala/code/model/ItemStateGrouper.scala
+++ src/main/scala/code/model/ItemStateGrouper.scala
@@ -4,10 +4,10 @@ import scala.collection.IndexedSeq
 
 /**
   * Created by philippederome on 2016-04-01.
-  */
-
-/**
   * Helper class that specifies which items represent required updates and required inserts
+  * @param updates sequence of items that should be updated
+  * @param inserts sequence of items that should be inserted
+  * @tparam T used for the type of items included in updated and inserts
   */
 case class UpdateAndInserts[T](updates: IndexedSeq[T], inserts: IndexedSeq[T])
 
@@ -16,9 +16,24 @@ case class UpdateAndInserts[T](updates: IndexedSeq[T], inserts: IndexedSeq[T])
   */
 trait ItemStateGrouper {
 
+  /**
+    * state of an associated Entity (Record here) as whether it requires change to database and cache
+    */
   sealed trait EntityRecordState
+
+  /**
+    * associated value is new, a candidate for insertion
+    */
   case object New extends EntityRecordState
+
+  /**
+    * associated value is dirty, a candidate for update
+    */
   case object Dirty extends EntityRecordState
+
+  /**
+    * associated value is clean/unchanged, nothing required to be done
+    */
   case object Clean extends EntityRecordState
 
   /**
@@ -27,6 +42,8 @@ trait ItemStateGrouper {
     * We want I to be an interface of T when using get/isDirty as get usage could be more abstract than type T at client side (possibly retrieving from cache).
     * @param items input items that are assumed to have valid data that we need to classify for caching strategy
     * @param get user method to retrieve what is in memory for the given item based on item's key
+    * @tparam I interface of T
+    * @tparam T type of elements of items
     * @return pairs of sequences as updates and inserts within UpdateAndInserts
     */
   def itemsByState[I, T <: I](items: IndexedSeq[T],
