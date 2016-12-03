3961c2b3837611faaf4a3d0bc54200cd27240515
Phil Derome <phderome@gmail.com>
2016-12-03 15:33:36 -0500

    merge RestClient!get into LCBOPageFetcher

--- src/main/scala/code/model/pageFetcher/LCBOPageFetcher.scala
+++ src/main/scala/code/model/pageFetcher/LCBOPageFetcher.scala
@@ -1,14 +1,15 @@
 package code.model.pageFetcher
 
-import code.Rest.RestClient
 import net.liftweb.json.{JNothing, JValue, parseOpt}
 import net.liftweb.common.Loggable
 import net.liftweb.util.Props
 import scala.annotation.tailrec
 import scala.collection.IndexedSeq
 import cats.implicits._
+import skinny.http.{HTTP, HTTPException, Request}
 
-trait LCBOPageFetcher extends RestClient with Loggable {
+trait LCBOPageFetcher extends Loggable {
+  import LCBOPageFetcher._
   type JSitemsExtractor[T] = JValue => IndexedSeq[T]
   type GotEnough_? = (Int) => Boolean
   type ValidateItems[T] = Either[Throwable, IndexedSeq[T]]
@@ -30,7 +31,7 @@ trait LCBOPageFetcher extends RestClient with Loggable {
                                 (implicit enough: GotEnough_? = neverEnough): ValidateItems[T] = Either.catchNonFatal {
     implicit val formats = net.liftweb.json.DefaultFormats
 
-    val uriRoot: String = s"http://${LCBOPageFetcher.LcboDomain}/$path"
+    val uriRoot: String = s"http://$LcboDomain/$path"
     // "go" is an idiom to use tailrec in Functional Programming in Scala as a helper function (and likewise using "closure" as is often found in JS).
     // Function would be pure if we'd bother to pass explicitly as params urlRoot, webApiRoute, xt, params, and enough, but conceptually it's the same.
     // It has no side effect for sure, other than helpful log.
@@ -58,9 +59,32 @@ trait LCBOPageFetcher extends RestClient with Loggable {
     val totalPages = (jsonRoot \ "pager" \ "total_pages").extractOrElse[Int](0)
     isFinalPage || totalPages < pageNo + 1
   }
+
+  /**
+    * Returns the text (content) from a REST URI as a String.
+    *
+    * @param uri A uri with full path and protocol but without query parameters, such as "http://foo.com/bar
+    * @param params a sequence of value pairs that will become ?param1=val1&param2=val2...&paramn=valn"
+    * @throws HTTPException
+    * @return a pair of JSON String obtained from REST site and the uri we just built
+    */
+  def get(uri: String, params: (String, Any)*): (String, String) = {
+    val r = Request(uri).queryParams(params: _*).
+      connectTimeoutMillis(connTimeOut).
+      readTimeoutMillis(sockTimeOut)
+    val response = HTTP.get(r)
+    if (response.status >= HTTP_PROTOCOL_GOOD_STAT_LOW && response.status < HTTP_PROTOCOL_GOOD_STAT_HI) {
+      (response.asString, r.toString)
+    } else {
+      throw new HTTPException(Some("Unexpected response "), response)
+    }
+  }
 }
 
 object LCBOPageFetcher {
   val LcboDomain: String = Props.get("lcboDomain", "")  // set it!
+  val HTTP_PROTOCOL_GOOD_STAT_LOW = 200
+  val HTTP_PROTOCOL_GOOD_STAT_HI = 300
+  val sockTimeOut = Props.getInt("http.ClientReadTimeOut", 0)
+  val connTimeOut = Props.getInt("http.ClientConnTimeOut", 0)
 }
-
