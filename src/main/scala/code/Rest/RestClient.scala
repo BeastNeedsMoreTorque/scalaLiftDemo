d435fed72af92a9a129b4228997bd9388a1e2d97
Phil Derome <phderome@gmail.com>
2016-12-04 12:37:34 -0500

    touch up

--- src/main/scala/code/Rest/RestClient.scala
+++ src/main/scala/code/Rest/RestClient.scala
@@ -1,3 +1,30 @@
 package code.Rest
 
-object RestClient
+import net.liftweb.util.Props
+import skinny.http.{HTTP, HTTPException, Request}
+
+object RestClient {
+  val HTTP_PROTOCOL_GOOD_STAT_LOW = 200
+  val HTTP_PROTOCOL_GOOD_STAT_HI = 300
+  val sockTimeOut = Props.getInt("http.ClientReadTimeOut", 0)
+  val connTimeOut = Props.getInt("http.ClientConnTimeOut", 0)
+  /**
+    * Returns the text (content) from a REST URI as a String.
+    *
+    * @param uri A uri with full path and protocol but without query parameters, such as "http://foo.com/bar
+    * @param params a sequence of value pairs that will become ?param1=val1&param2=val2...&paramn=valn"
+    * @throws HTTPException
+    * @return a pair of JSON String obtained from REST site and the uri we just built
+    */
+  def get(uri: String, params: (String, Any)*): (String, String) = {
+    val r = Request(uri).queryParams(params: _*).
+      connectTimeoutMillis(connTimeOut).
+      readTimeoutMillis(sockTimeOut)
+    val response = HTTP.get(r)
+    if (response.status >= HTTP_PROTOCOL_GOOD_STAT_LOW && response.status < HTTP_PROTOCOL_GOOD_STAT_HI) {
+      (response.asString, r.toString)
+    } else {
+      throw new HTTPException(Some("Unexpected response "), response)
+    }
+  }
+}
--- src/main/scala/code/model/pageFetcher/LCBOPageFetcher.scala
+++ src/main/scala/code/model/pageFetcher/LCBOPageFetcher.scala
@@ -1,12 +1,12 @@
 package code.model.pageFetcher
 
+import code.Rest.RestClient
 import net.liftweb.json.{JNothing, JValue, parseOpt}
 import net.liftweb.common.Loggable
 import net.liftweb.util.Props
 import scala.annotation.tailrec
 import scala.collection.IndexedSeq
 import cats.implicits._
-import skinny.http.{HTTP, HTTPException, Request}
 
 trait LCBOPageFetcher extends Loggable {
   import LCBOPageFetcher._
@@ -39,7 +39,7 @@ trait LCBOPageFetcher extends Loggable {
     @tailrec // in general it's good to make recursion tailrec to avoid stack overflow.
     def go(accumItems: IndexedSeq[T], currPage: Int): IndexedSeq[T] = {
       // get as many as possible on a page because we could have few matches.
-      val (msg, uri) = get(uriRoot, params ++ Seq(("page", currPage)): _*)
+      val (msg, uri) = RestClient.get(uriRoot, params ++ Seq(("page", currPage)): _*)
       val jsonRoot = parseOpt(msg).fold[JValue](JNothing)(identity)
       // fyi: throws plenty of various exceptions.
       val revisedItems = accumItems ++ xt(jsonRoot \ "result") // Uses XPath-like querying to extract data from parsed object jsObj.
@@ -59,32 +59,8 @@ trait LCBOPageFetcher extends Loggable {
     val totalPages = (jsonRoot \ "pager" \ "total_pages").extractOrElse[Int](0)
     isFinalPage || totalPages < pageNo + 1
   }
-
-  /**
-    * Returns the text (content) from a REST URI as a String.
-    *
-    * @param uri A uri with full path and protocol but without query parameters, such as "http://foo.com/bar
-    * @param params a sequence of value pairs that will become ?param1=val1&param2=val2...&paramn=valn"
-    * @throws HTTPException
-    * @return a pair of JSON String obtained from REST site and the uri we just built
-    */
-  def get(uri: String, params: (String, Any)*): (String, String) = {
-    val r = Request(uri).queryParams(params: _*).
-      connectTimeoutMillis(connTimeOut).
-      readTimeoutMillis(sockTimeOut)
-    val response = HTTP.get(r)
-    if (response.status >= HTTP_PROTOCOL_GOOD_STAT_LOW && response.status < HTTP_PROTOCOL_GOOD_STAT_HI) {
-      (response.asString, r.toString)
-    } else {
-      throw new HTTPException(Some("Unexpected response "), response)
-    }
-  }
 }
 
 object LCBOPageFetcher {
   val LcboDomain: String = Props.get("lcboDomain", "")  // set it!
-  val HTTP_PROTOCOL_GOOD_STAT_LOW = 200
-  val HTTP_PROTOCOL_GOOD_STAT_HI = 300
-  val sockTimeOut = Props.getInt("http.ClientReadTimeOut", 0)
-  val connTimeOut = Props.getInt("http.ClientConnTimeOut", 0)
 }
